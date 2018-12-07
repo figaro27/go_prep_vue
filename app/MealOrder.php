e6ab318cbca66e59b82c58532a48179ad13706f4
figaro27 <sweetfigaro27@gmail.com>
2018-12-07 18:31:27 -0500

    Meals / Meal Order connection, pulling meals into child row, starting delivery notes

--- app/MealOrder.php
+++ app/MealOrder.php
@@ -9,20 +9,11 @@ class MealOrder extends Pivot
 {
   protected $table = 'meal_orders';
 
-  public function meal() {
+  public function meals() {
 		return $this->hasOne('App\Meal');
 	}
 
 	public function orders() {
 		return $this->hasMany('App\Order');
 	}
-
-	//This does not work yet
-// 	public function getMealNameAttribute(){
-// 		foreach ($this->meal->title as $title) {
-//     	return "{$title}";
-//     }
-// }
-
-//     protected $appends = ['meal_name'];
 }
