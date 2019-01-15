2592460ec857e5f409f354b9ffdf3e8873a22144
figaro27 <sweetfigaro27@gmail.com>
2019-01-15 14:55:49 -0500

    Fixing meal category seeder and Vue Tables 2 from fork

--- database/factories/MealCategoryFactory.php
+++ database/factories/MealCategoryFactory.php
@@ -3,10 +3,9 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\MealCategory::class, function (Faker $faker) {
-    $category = array("Breakfast", "Lunch", "Dinner");
     return [
         'meal_id' => rand(1, 50),
-        'category' => $category[rand(0, 2)],
+        'store_category_id' => rand(0, 2),
         'created_at' => $faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now'),
     ];
 });
