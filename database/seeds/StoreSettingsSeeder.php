a42380b30321475612c1fa6c9278d1ce5bceb26b
figaro27 <sweetfigaro27@gmail.com>
2019-01-15 00:43:51 -0200

    JSON-encode notifications column

--- database/seeds/StoreSettingsSeeder.php
+++ database/seeds/StoreSettingsSeeder.php
@@ -23,12 +23,12 @@ class StoreSettingsSeeder extends Seeder
             'delivery_distance_zipcodes' => '[10000,10001,10002]',
             'stripe_id' => 'acct_1DsNh6GjtVvEbWjh',
             'stripe_account' => '{"access_token":"sk_test_hKtcOHTpFM8KdQLjBlvYqTr8","livemode":false,"refresh_token":"rt_EL3ofmpyZNiFniC7w3PUahgbjHQf0sImSTs0mkKMssZr2G2p","token_type":"bearer","stripe_publishable_key":"pk_test_QqLPuymz1GAGAbkLUkEMnTaO","stripe_user_id":"acct_1DsNh6GjtVvEbWjh","scope":"express"}',
-            'notifications' => [
+            'notifications' => json_encode([
                 'new_orders' => true,
                 'new_subscriptions' => true,
                 'cancelled_subscriptions' => true,
                 'ready_to_print' => true,
-            ],
+            ]),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
