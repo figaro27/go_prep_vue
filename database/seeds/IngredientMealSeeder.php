b43a7aeb09069cd5dc4a0f7efb3a89c5a4064e39
figaro27 <sweetfigaro27@gmail.com>
2019-02-05 02:07:35 -0200

    Fix IngredientMealSeeder

--- database/seeds/IngredientMealSeeder.php
+++ database/seeds/IngredientMealSeeder.php
@@ -11,21 +11,21 @@ class IngredientMealSeeder extends Seeder
      */
     public function run()
     {
-    	$unitTypes = [
-                'mass' => ['oz', 'g'],
-                'volume' => ['ml', 'tsp', 'fl-oz'],
-                'unit' => ['unit'],
-            ];
+        $unitTypes = [
+            'mass' => ['oz', 'g'],
+            'volume' => ['ml', 'tsp', 'fl-oz'],
+            'unit' => ['unit'],
+        ];
         $unitUnits = ['pieces', 'pinches', 'drops'];
 
-        for($i=0;$i<20;$i++){
-		        DB::table('ingredient_meal')->insert([
-		            'ingredient_id' => $i,
-		            'meal_id' => $i,
-		            'quantity' => rand(1,15),
-		            'quantity_unit' => 'oz',
-		            'quantity_unit_display' => 'pinches'
-		        ]);
- 		}
+        for ($i = 1; $i < 20; $i++) {
+            DB::table('ingredient_meal')->insert([
+                'ingredient_id' => $i,
+                'meal_id' => $i,
+                'quantity' => rand(1, 15),
+                'quantity_unit' => 'oz',
+                'quantity_unit_display' => 'pinches',
+            ]);
+        }
     }
 }
