1e4c021c76ec295d2907ab64df3175c58bce1b24
figaro27 <sweetfigaro27@gmail.com>
2019-02-11 22:55:40 -0200

    Fixing subscription seeder

--- database/seeds/SubscriptionsSeeder.php
+++ database/seeds/SubscriptionsSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+use App\User;
 
 class SubscriptionsSeeder extends Seeder
 {
@@ -12,42 +14,43 @@ class SubscriptionsSeeder extends Seeder
      */
     public function run()
     {
-    	        DB::table('subscriptions')->insert([
-    	        	'user_id' => 3,
-    	        	'customer_id' => 3,
-    	        	'store_id' => 1,
-    	        	'name' => 'name',
-    	        	'status' => 'active',
-    	        	'stripe_id' => substr(md5(rand()), 0, 13),
-    	        	'stripe_customer_id' => 1,
-    	        	'stripe_plan' => 1,
-    	        	'quantity' => 1,
-    	        	'amount' => mt_rand(1200, 3000) / 10,
-    	        	'interval' => 'week',
-    	        	'delivery_day' => rand(1,7),
-    	        	'created_at' => Carbon::now(),
-    	            'updated_at' => Carbon::now()
-    	        ]);
+        $subId = DB::table('subscriptions')->insertGetId([
+            'user_id' => 3,
+            'customer_id' => 3,
+            'store_id' => 1,
+            'name' => 'name',
+            'status' => 'active',
+            'stripe_id' => substr(md5(rand()), 0, 13),
+            'stripe_customer_id' => 1,
+            'stripe_plan' => 1,
+            'quantity' => 1,
+            'amount' => mt_rand(1200, 3000) / 10,
+            'interval' => 'week',
+            'delivery_day' => rand(1, 7),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        $user = User::find(3);
+        $user->orders()->first()->update(['subscription_id' => $subId]);
 
-                for($i=13;$i<23;$i++){
-                    DB::table('subscriptions')->insert([
-                        'user_id' => $i,
-                        'customer_id' => $i,
-                        'store_id' => 1,
-                        'name' => 'name',
-                        'status' => 'active',
-                        'stripe_id' => substr(md5(rand()), 0, 13),
-                        'stripe_customer_id' => 1,
-                        'stripe_plan' => 1,
-                        'quantity' => 1,
-                        'amount' => mt_rand(1200, 3000) / 10,
-                        'interval' => 'week',
-                        'delivery_day' => rand(4,7),
-                        'created_at' => Carbon::now()->subDays($i),
-                        'updated_at' => Carbon::now()
-                    ]);
-            }
-            
-            
+        for ($i = 13; $i < 23; $i++) {
+            DB::table('subscriptions')->insert([
+                'user_id' => $i,
+                'customer_id' => $i,
+                'store_id' => 1,
+                'name' => 'name',
+                'status' => 'active',
+                'stripe_id' => substr(md5(rand()), 0, 13),
+                'stripe_customer_id' => 1,
+                'stripe_plan' => 1,
+                'quantity' => 1,
+                'amount' => mt_rand(1200, 3000) / 10,
+                'interval' => 'week',
+                'delivery_day' => rand(4, 7),
+                'created_at' => Carbon::now()->subDays($i),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+
     }
 }
--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -23,34 +23,46 @@ class UsersSeeder extends Seeder
         ]);
 
         DB::table('users')->insert([
-          'user_role_id' => 1,
-          'email' => 'dan.j.barbosa@gmail.com',
-          'email_verified_at' => now(),
-          'password' => bcrypt('secret'),
-          'remember_token' => str_random(10),
-          'created_at' => now(),
-          'updated_at' => now(),
+            'user_role_id' => 1,
+            'email' => 'dan.j.barbosa@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('secret'),
+            'remember_token' => str_random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         DB::table('users')->insert([
-          'user_role_id' => 1,
-          'email' => 'customer@goprep.com',
-          'email_verified_at' => now(),
-          'password' => bcrypt('secret'),
-          'remember_token' => str_random(10),
-          'created_at' => now(),
-          'updated_at' => now(),
-      ]);
+            'user_role_id' => 1,
+            'email' => 'customer@goprep.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('secret'),
+            'remember_token' => str_random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
 
-      DB::table('users')->insert([
-          'user_role_id' => 3,
-          'email' => 'admin@goprep.com',
-          'email_verified_at' => now(),
-          'password' => bcrypt('secret'),
-          'remember_token' => str_random(10),
-          'created_at' => now(),
-          'updated_at' => now(),
-      ]);
+        $customer = new App\Customer();
+        $customer->user_id = 3;
+        $customer->store_id = 1;
+        $customer->stripe_id = '';
+        $customer->save();
+
+        $order = App\User::find(3)->orders()->save(
+          factory(App\Order::class)->make([
+            'customer_id' => $customer->id
+          ])
+        );
+
+        DB::table('users')->insert([
+            'user_role_id' => 3,
+            'email' => 'admin@goprep.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('secret'),
+            'remember_token' => str_random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
 
         for ($i = 2; $i < 11; $i++) {
             DB::table('users')->insert([
@@ -67,17 +79,17 @@ class UsersSeeder extends Seeder
         factory(App\User::class, 50)->create()->each(function ($u) {
             $u->userDetail()->save(factory(App\UserDetail::class)->make());
 
-                $customer = new App\Customer();
-                $customer->user_id = $u->id;
-                $customer->store_id = 1;
-                $customer->stripe_id = '';
-                $customer->save();
+            $customer = new App\Customer();
+            $customer->user_id = $u->id;
+            $customer->store_id = 1;
+            $customer->stripe_id = '';
+            $customer->save();
 
-                $order = $u->Order()->save(
-                  factory(App\Order::class)->make([
-                    'customer_id' => $customer->id
-                  ])
-                );
+            $order = $u->Order()->save(
+                factory(App\Order::class)->make([
+                    'customer_id' => $customer->id,
+                ])
+            );
 
         });
 
