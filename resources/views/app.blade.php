09532880442bfc25d3cf4335dfcebbe0f251b591
figaro27 <sweetfigaro27@gmail.com>
2019-02-18 01:28:38 -0300

    V1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 <title>GoPrep</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ config('app.url').'/css/app.css' }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
