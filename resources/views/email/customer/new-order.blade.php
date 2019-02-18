724662afb3e3e2969a731cf374bcb7a8ca107779
figaro27 <sweetfigaro27@gmail.com>
2019-02-18 12:27:06 -0500

    New Order notification updates

--- resources/views/email/customer/new-order.blade.php
+++ resources/views/email/customer/new-order.blade.php
@@ -1,7 +1,35 @@
-New order!
+<div class="card">
+	<div class="card-body">
+		<h4>Thank you for your order from {{ $order->store_name }}.</h4>
+		<p>Your meals will be prepped and delivered to you on {{ $order->delivery_date }}</p>
+		<p>Please contact {{ $order->store_name }} for any issues at {{ $order->store->store_detail->phone }}</p>
+		<h5>Order Details</h5>
+		<p>Subtotal: {{ $order->preFeePreDiscount }}</p>
+		<p>Meal Plan Discount: <span class="red"> ({{ $order->mealPlanDiscount }})</span></p>
+		<p>Delivery Fee: {{ $order->deliveryFee }}</p>
+		<p>Processing Fee: {{ $order->processingFee) }}</p>
+		<p><strong>Total: {{ $order->amount }}</strong></p>
 
-{{ $order->amount }}
-<br><br><br><br>
-{{ $order->store_name }}
+
+	</div>
+</div>
+
+
 
 <!-- {{ $subscription }} -->
+
+
+
+<!-- 
+<li class="checkout-item">
+	<div class="row">
+	  <div class="col-md-4">
+	    <strong>Meal Plan Discount:</strong>
+	  </div>
+	  <div class="col-md-3 offset-5">
+	  	{{ format.money(mealPlanDiscount) }}
+	  </div>
+	</div>
+</li>
+ -->
+
