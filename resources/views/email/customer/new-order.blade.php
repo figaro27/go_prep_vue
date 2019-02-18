5bb7b79ecb7a641d0b57b08f55986ad06a01a4a0
figaro27 <sweetfigaro27@gmail.com>
2019-02-18 12:54:17 -0500

    1

--- resources/views/email/customer/new-order.blade.php
+++ resources/views/email/customer/new-order.blade.php
@@ -3,6 +3,12 @@
 		<h4>Thank you for your order from {{ $order->store_name }}</h4>
 		<p>Your meals will be prepped and delivered to you on {{ $order->delivery_date }}</p>
 
+		<h5>Order Details</h5>
+		<p>Subtotal: {{ $order->preFeePreDiscount }}</p>
+		<p>Meal Plan Discount: <span class="red"> ({{ $order->mealPlanDiscount }})</span></p>
+		<p>Delivery Fee: {{ $order->deliveryFee }}</p>
+		<p>Processing Fee: {{ $order->processingFee }}</p>
+		<p><strong>Total: {{ $order->amount }}</strong></p>
 
 
 	</div>
