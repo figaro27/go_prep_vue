f865a17898c4a4d1540c0ec7f01f2ba732ea11db
figaro27 <sweetfigaro27@gmail.com>
2019-02-18 12:49:43 -0500

    1

--- resources/views/email/customer/new-order.blade.php
+++ resources/views/email/customer/new-order.blade.php
@@ -1,35 +1,3 @@
-<!-- <div class="card">
-	<div class="card-body"> -->
+
 		{{ $order->store_name }}
-		<!-- {{ $order->delivery_date }} -->
-		<!-- <p>Please contact {{ $order->store_name }} for any issues at {{ $order->store->store_detail->phone }}</p> -->
-
-<!-- 		{{ $order->preFeePreDiscount }}
-		{{ $order->mealPlanDiscount }}
-		{{ $order->deliveryFee }}
-		{{ $order->processingFee }}
-		{{ $order->amount }} -->
-
-
-<!-- 	</div>
-</div>
- -->
-
-
-<!-- {{ $subscription }} -->
-
-
-
-<!-- 
-<li class="checkout-item">
-	<div class="row">
-	  <div class="col-md-4">
-	    <strong>Meal Plan Discount:</strong>
-	  </div>
-	  <div class="col-md-3 offset-5">
-	  	{{ format.money(mealPlanDiscount) }}
-	  </div>
-	</div>
-</li>
- -->
-
+		{{ $order->delivery_date }}
