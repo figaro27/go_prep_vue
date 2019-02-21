735e01a5bd3d136c79349376a68aa4368c1c6c7a
figaro27 <sweetfigaro27@gmail.com>
2019-02-21 12:16:42 -0500

    Test

--- resources/assets/js/views/Store/Account/Settings.vue
+++ resources/assets/js/views/Store/Account/Settings.vue
@@ -426,7 +426,7 @@
         </div>
       </div>
 
-      <p>Open</p>
+      <p>Open - TEST</p>
       <div class="card">
         <div class="card-body">
           <b-form @submit.prevent="updateStoreSettings" v-if="canOpen">
