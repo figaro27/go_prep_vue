02685668cfc85f0e953e075e92f696668fb9f798
figaro27 <sweetfigaro27@gmail.com>
2019-02-21 00:06:55 -0300

    Enhancements to store logo upload handler

--- app/Http/Controllers/User/StoreDetailController.php
+++ app/Http/Controllers/User/StoreDetailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\StoreDetail;
 use Auth;
 use Illuminate\Http\Request;
+use \App\Utils\Images;
 
 class StoreDetailController extends Controller
 {
@@ -72,27 +73,48 @@ class StoreDetailController extends Controller
      */
     public function update(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|string',
-            'logo' => 'required|string',
-            'phone' => 'required|string',
-            'address' => 'required|string',
-            'city' => 'required|string',
-            'state' => 'required|string',
-            'zip' => 'required|numeric',
-            'description' => 'required|string|max:450',
-            'logo' => 'dimensions:ratio=1/1'
-        ]);
+      $id = auth('api')->user()->id;
+      $store = StoreDetail::findOrFail($id);
 
-        $id = auth('api')->user()->id;
-        $store = StoreDetail::findOrFail($id);
+        $rules = [
+          'name' => 'required|string',
+          'logo' => 'required|string',
+          'phone' => 'required|string',
+          'address' => 'required|string',
+          'city' => 'required|string',
+          'state' => 'required|string',
+          'zip' => 'required|numeric',
+          'description' => 'required|string|max:450',
+        ];
+
+        $this->validate($request, $rules);
+
+        $newLogo = $request->has('logo') && substr($request->get('logo'), 0, 4) === 'data';
+
+        if($newLogo) {
+          $image = Images::decodeB64($request->get('logo'));
+          $size = getimagesizefromstring($image);
+
+          if($size && $size[0] !== $size[1]) {
+            return response()->json([
+              'message' => 'The given data was invalid.',
+              'errors' => [
+                'logo' => ['The logo must have an equal width and height.']
+              ]
+            ], 422);
+          }
+        }
+
 
         $store->update($request->except('logo'));
 
-        if ($request->has('logo')) {
-          $imageUrl = \App\Utils\Images::uploadB64($request->get('logo'));
-          $store->logo = $imageUrl;
-          $store->save();
+        if ($newLogo) {
+          $imageUrl = Images::uploadB64($request->get('logo'));
+
+          if($imageUrl) {
+            $store->logo = $imageUrl;
+            $store->save();
+          }
         }
 
         return $store;
--- app/Utils/Images.php
+++ app/Utils/Images.php
@@ -4,22 +4,33 @@ namespace App\Utils;
 
 class Images {
   public static function uploadB64($imageRaw, $return = 'url', $filenamePrefix = '_') {
-    if (!\Storage::exists($imageRaw)) {
-      $imageRaw = str_replace(' ', '+', $imageRaw);
+    if (!\Storage::exists($imageRaw) && $imageRaw[0] !== '/') {
+      $image = self::decodeB64($imageRaw);
       
-      $ext = [];
-      preg_match('/^data:image\/(.{3,9});base64,/i', $imageRaw, $ext);
-      
-      if (count($ext) > 1) {
-        $image = substr($imageRaw, strlen($ext[0]));
-        $image = base64_decode($image);
-
+      if ($image) {
+        $ext = [];
+        preg_match('/^data:image\/(.{3,9});base64,/i', $imageRaw, $ext);
+        
         $imagePath = 'public/images/stores/' . $filenamePrefix . sha1($image) . '.' . $ext[1];
-          \Storage::disk('local')->put($imagePath, $image);
-          $imageUrl = \Storage::url($imagePath);
+        \Storage::disk('local')->put($imagePath, $image);
+        $imageUrl = \Storage::url($imagePath);
 
-          return $imageUrl;
+        return $imageUrl;
       }
     }
+  }
+
+  public static function decodeB64($imageRaw) {
+    $imageRaw = str_replace(' ', '+', $imageRaw);
+      
+    $ext = [];
+    preg_match('/^data:image\/(.{3,9});base64,/i', $imageRaw, $ext);
+    
+    if (count($ext) > 1) {
+      $image = substr($imageRaw, strlen($ext[0]));
+      $image = base64_decode($image);
+      return $image;
+    }
+    return null;
   }
 }
