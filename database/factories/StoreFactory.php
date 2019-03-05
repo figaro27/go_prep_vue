ded100a187b0ac26056c398c686053ae2caee2ad
figaro27 <sweetfigaro27@gmail.com>
2019-03-05 13:28:51 -0300

    +1

--- database/factories/StoreFactory.php
+++ database/factories/StoreFactory.php
@@ -5,7 +5,8 @@ use Faker\Generator as Faker;
 $factory->define(App\Store::class, function (Faker $faker) {
 	static $i = 1;
     return [
-    	'user_id' => $i++,
-        'created_at' => $faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now')
+      'user_id' => $i++,
+      'accepted_toa' => 1,
+      'created_at' => $faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now')
     ];
 });
