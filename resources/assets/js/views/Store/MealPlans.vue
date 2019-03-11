4c17d2630784f79917d4b74ec51dc7eb2606b950
figaro27 <sweetfigaro27@gmail.com>
2019-03-11 12:56:50 -0400

    +1

--- resources/assets/js/views/Store/MealPlans.vue
+++ resources/assets/js/views/Store/MealPlans.vue
@@ -184,7 +184,14 @@ export default {
           return classes;
         },
         customSorting: {
-
+          created_at: function(ascending) {
+            return function(a, b) {
+              var numA = moment(a.created_at);
+              var numB = moment(b.created_at);
+              if (ascending) return numA.isBefore(numB, "day") ? 1 : -1;
+              return numA.isAfter(numB, "day") ? 1 : -1;
+            };
+          },
           delivery_day: function(ascending) {
             return function(a, b) {
               var numA = moment(a.delivery_day);
@@ -194,6 +201,9 @@ export default {
             };
           }
         },
+        orderBy: { 
+          column:'created_at' 
+        }
       },
       deliveryNote: ""
     };
