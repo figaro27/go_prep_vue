66283399b06331b119c41e3ff15524f12ecc6158
figaro27 <sweetfigaro27@gmail.com>
2019-03-17 21:47:25 -0400

    Test

--- resources/assets/js/views/Store/Menu.vue
+++ resources/assets/js/views/Store/Menu.vue
@@ -4,6 +4,7 @@
       <div class="col-md-12">
         <div class="card">
           <div class="card-body">
+            Test Dev Connection
             <Spinner v-if="isLoading"/>
 
             <v-client-table
