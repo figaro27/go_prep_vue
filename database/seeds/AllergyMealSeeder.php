a7b88042d3f73e113038f92b4882943ba37eb592
figaro27 <sweetfigaro27@gmail.com>
2019-03-21 17:38:19 -0400

    Allergy seeder improvement

--- database/seeds/AllergyMealSeeder.php
+++ database/seeds/AllergyMealSeeder.php
@@ -11,12 +11,59 @@ class AllergyMealSeeder extends Seeder
      */
     public function run()
     {
-        for($i=1;$i<=20;$i++){
+
         DB::table('allergy_meal')->insert([
-            'meal_id' => $i,
-            'allergy_id' => rand(1,8)
+            'meal_id' => 1,
+            'allergy_id' => 7
         ]);
-    	}
+    	DB::table('allergy_meal')->insert([
+            'meal_id' => 2,
+            'allergy_id' => 2
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 3,
+            'allergy_id' => 2
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 4,
+            'allergy_id' => 6
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 6,
+            'allergy_id' => 4
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 6,
+            'allergy_id' => 7
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 9,
+            'allergy_id' => 6
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 10,
+            'allergy_id' => 7
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 10,
+            'allergy_id' => 3
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 11,
+            'allergy_id' => 7
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 13,
+            'allergy_id' => 2
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 14,
+            'allergy_id' => 4
+        ]);
+        DB::table('allergy_meal')->insert([
+            'meal_id' => 16,
+            'allergy_id' => 4
+        ]);
 
     	
     }
