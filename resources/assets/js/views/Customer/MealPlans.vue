6172b684f5ef4c2d4ce0345a3d8eab28c6d25769
figaro27 <sweetfigaro27@gmail.com>
2019-03-20 22:32:26 -0400

    Margin between customer meal plans

--- resources/assets/js/views/Customer/MealPlans.vue
+++ resources/assets/js/views/Customer/MealPlans.vue
@@ -23,7 +23,7 @@
         <div class="card-body">
           <Spinner v-if="isLoading"/>
           <div class="order-list">
-            <div v-for="subscription in subscriptions" :key="subscription.id">
+            <div v-for="subscription in subscriptions" :key="subscription.id" class="mb-4">
               <div v-b-toggle="'collapse' + subscription.id">
                 <b-list-group-item class="order-list-item">
                   <div class="row">
