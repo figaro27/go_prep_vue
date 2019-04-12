b045eb7f9d58ac26a21ff11648caffc9f588d213
figaro27 <sweetfigaro27@gmail.com>
2019-04-12 15:08:12 -0400

    Removing unpaid orders

--- app/Order.php
+++ app/Order.php
@@ -38,7 +38,7 @@ class Order extends Model
 
     public function customer()
     {
-        return $this->belongsTo('App\Customer');
+        return $this->belongsTo('App\Customer')->where('paid', 1);
     }
 
     public function store()
