459512b47467cb8b50bb1ea06e6474324ba97e3b
figaro27 <sweetfigaro27@gmail.com>
2019-04-27 12:03:52 -0400

    Delivery route report progress

--- app/Http/Controllers/DeliveryRouteController.php
+++ app/Http/Controllers/DeliveryRouteController.php
@@ -7,37 +7,39 @@ use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use App\StoreDetail;
 use App\Order;
+use App\Store;
 
 class DeliveryRouteController extends Controller
 {
     public function getRoutes()
     {
         $googleApiKey = 'AIzaSyArp-lohyOEQXF6a69wyFXruthJd9jNY4U';
+        $hereApp_id = "V2tJJFOIa2LjoSw4xNuX";
+        $hereApp_code = "JRGmnV2itkv7cCLRWc55CA";
+        // $hereApp_id="D2vwjQLe6hZEsNkdzPf0";
+        // $hereApp_code="_0IsSILsI4W-7piSDVl81A";
 
-        $client = new Client([
-            'base_uri' => 'https://maps.googleapis.com/maps/api/directions/'
-        ]);
+        // Get all customer addresses from orders
 
-        // Get the correct store - move controller to Store folder? And to routes/api.
-        $id = Auth::user()->id;
-        $store = Store::where('user_id', $id)
-            ->pluck('id')
-            ->first();
+        $id = auth('api')->user()->id;
+        $store = Store::where('user_id', $id)->first();
+        $storeDetails = $store->details;
         $address =
-            $store->address .
+            $storeDetails->address .
             ' ' .
-            $store->city .
+            $storeDetails->city .
             ' ' .
-            $store->state .
+            $storeDetails->state .
             ' ' .
-            $store->zip;
+            $storeDetails->zip;
         $storeAddress = str_replace(' ', '+', $address);
 
         $customerAddresses = [];
+        $customerAddressesNatural = [];
 
-        // Change to only orders for the requested delivery date(s);
-        // Google only allows 23 Waypoints at a time. Need to do this in multiple requests
-        $orders = Order::where('store_id', $store->id)->take(20);
+        // Transfer to Exportable and get delivery dates for accurate orders
+
+        $orders = Order::where('store_id', $store->id)->get();
 
         foreach ($orders as $order) {
             $customerAddress = $order->user->details;
@@ -51,24 +53,97 @@ class DeliveryRouteController extends Controller
                     '+' .
                     $customerAddress->zip
             );
+
+            array_push(
+                $customerAddressesNatural,
+                $customerAddress->address .
+                    ' ' .
+                    $customerAddress->city .
+                    ' ' .
+                    $customerAddress->state .
+                    ' ' .
+                    $customerAddress->zip
+            );
         }
 
-        $customerAddresses = implode("|", $customerAddresses);
+        // Convert store address to geocode
 
-        $res = $client->get(
-            'json?origin=' .
-                $storeAddress .
-                '&destination=' .
-                $storeAddress .
-                '&waypoints=optimize:true|' .
-                $customerAddresses .
-                '
-        |&key=' .
+        $googleClient = new Client();
+
+        $storeCoordinates = '';
+        $res = $googleClient->get(
+            'https://maps.googleapis.com/maps/api/geocode/json?address=' .
+                $customerAddress .
+                '&key=' .
                 $googleApiKey
         );
+        $response = $res->getBody();
+        $body = json_decode($response);
+        $latitude = $body->results[0]->geometry->location->lat;
+        $longitude = $body->results[0]->geometry->location->lng;
+        $storeCoordinates .= $latitude . ',' . $longitude;
 
-        return $res->getBody();
+        // Convert customer addresses to geocodes
 
-        // Output into a report.
+        $coordinates = [];
+
+        foreach ($customerAddresses as $customerAddress) {
+            $res = $googleClient->get(
+                'https://maps.googleapis.com/maps/api/geocode/json?address=' .
+                    $customerAddress .
+                    '&key=' .
+                    $googleApiKey
+            );
+            $response = $res->getBody();
+            $body = json_decode($response);
+            $latitude = $body->results[0]->geometry->location->lat;
+            $longitude = $body->results[0]->geometry->location->lng;
+            array_push($coordinates, $latitude . ',' . $longitude);
+            sleep(0.25);
+        }
+
+        // Append all geocoded addresses together
+
+        $hereClient = new Client();
+
+        $i = 1;
+        $coordinatesString = '&destination' . $i . '=';
+        $len = count($coordinates);
+
+        foreach ($coordinates as $coordinate) {
+            $i++;
+            $coordinatesString .= $coordinate;
+            if ($i != $len + 1) {
+                $coordinatesString .= '&destination' . $i . '=';
+            }
+        }
+
+        // Get the optimal delivery route order
+
+        $res = $hereClient->get(
+            'https://wse.api.here.com/2/findsequence.json?start=Start;' .
+                $storeCoordinates .
+                $coordinatesString .
+                '&mode=fastest;car&app_id=' .
+                $hereApp_id .
+                '&app_code=' .
+                $hereApp_code
+        );
+
+        $response = $res->getBody();
+
+        $body = json_decode($response);
+
+        $waypoints = $body->results[0]->waypoints;
+
+        $order = [];
+
+        foreach ($waypoints as $waypoint) {
+            array_push($order, $waypoint->id);
+        }
+
+        // Fix this below, maybe get delivery instructions, get accurate delivery date for orders, and should be done
+        $output = array_merge($customerAddressesNatural, $order);
+        return $output;
     }
 }
