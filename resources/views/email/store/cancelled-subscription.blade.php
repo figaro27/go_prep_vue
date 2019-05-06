6d5f7fa72e01908c33d97c66a84cb05cd416e065
figaro27 <sweetfigaro27@gmail.com>
2019-05-06 11:40:30 -0400

    Missed one

--- resources/views/email/store/cancelled-subscription.blade.php
+++ resources/views/email/store/cancelled-subscription.blade.php
@@ -246,6 +246,7 @@ u + .body .full { width:100% !important; width:100vw !important;}
                         $deliveryFee = $subscription->deliveryFee;
                         $processingFee = $subscription->processingFee;
                         $salesTax = $subscription->salesTax;
+                        $coupon = $subscription->couponReduction;
                         @endphp
 
                         @if ($mealPlanDiscount > 0 || $deliveryFee > 0 || $processingFee > 0)
@@ -260,7 +261,10 @@ u + .body .full { width:100% !important; width:100vw !important;}
                         @if ($processingFee > 0)
                         Processing Fee<br>
                         @endif
-                        Sales Tax<br><br>
+                        Sales Tax<br>
+                        @if ($coupon > 0)
+                        Coupon<br>
+                        @endif<br><br>
                         <span style="font-family: 'Open Sans', Arial, sans-serif; font-size:24px; color:#3b3b3b;  font-weight: bold;">Total</span>
                       </td>
 
@@ -270,7 +274,7 @@ u + .body .full { width:100% !important; width:100vw !important;}
                           ${{ number_format($subtotal, 2) }}<br>
                           @endif
                           @if ($mealPlanDiscount > 0)
-                          ${{ number_format($mealPlanDiscount, 2) }}<br>
+                          (${{ number_format($mealPlanDiscount, 2) }})<br>
                           @endif
                           @if ($deliveryFee > 0)
                           ${{ number_format($deliveryFee, 2) }}<br>
@@ -278,7 +282,10 @@ u + .body .full { width:100% !important; width:100vw !important;}
                           @if ($processingFee > 0)
                           ${{ number_format($processingFee, 2) }}<br>
                           @endif
-                          ${{ number_format($salesTax, 2) }}<br><br>
+                          ${{ number_format($salesTax, 2) }}<br>
+                          @if ($coupon > 0)
+                          (${{ number_format($coupon, 2) }})<br>
+                          @endif<br><br>
                           <span style="font-family: 'Open Sans', Arial, sans-serif; font-size:24px; color:#3b3b3b;  font-weight: bold;">${{ number_format($subscription->amount, 2) }}</span>
                         </td>
                       </tr>
