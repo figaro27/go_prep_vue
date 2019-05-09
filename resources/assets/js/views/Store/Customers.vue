771fcc3688d1ddc3ecd9884f1275a8e38056b68a
figaro27 <sweetfigaro27@gmail.com>
2019-05-09 10:46:21 -0400

    Price breakdown on store/customers orders

--- resources/assets/js/views/Store/Customers.vue
+++ resources/assets/js/views/Store/Customers.vue
@@ -100,7 +100,44 @@
                   </p>
                 </div>
                 <div class="col-md-3">
-                  <h2>{{ format.money(order.amount) }}</h2>
+                  <span>
+                    Subtotal: {{ format.money(order.preFeePreDiscount) }} </span
+                  ><br />
+                  <span v-if="order.mealPlanDiscount > 0">
+                    Meal Plan Discount:
+                    <span class="text-success"
+                      >({{ format.money(order.mealPlanDiscount) }})
+                    </span>
+                    <br />
+                  </span>
+                  <span v-if="order.deliveryFee > 0">
+                    Delivery Fee: {{ format.money(order.deliveryFee) }}
+                    <br />
+                  </span>
+                  <span v-if="order.processingFee > 0">
+                    Processing Fee:
+                    {{ format.money(order.processingFee) }}
+                    <br />
+                  </span>
+                  <span>Sales Tax: {{ format.money(order.salesTax) }}</span
+                  ><br />
+                  <span>
+                    <strong
+                      ><span v-if="order.couponReduction === null"
+                        >Total: {{ format.money(order.amount) }}</span
+                      ></strong
+                    >
+                    <div v-if="order.couponReduction > 0">
+                      Pre-Coupon Total: {{ format.money(order.pre_coupon) }}
+                      <br />
+                      <span class="text-success"
+                        >(Coupon {{ order.couponCode }}:
+                        {{ format.money(order.couponReduction) }})</span
+                      >
+                      <br />
+                      <strong>Total: {{ format.money(order.amount) }}</strong>
+                    </div>
+                  </span>
                 </div>
                 <div class="col-md-1">
                   <img
