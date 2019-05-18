8025347fbbebb4d9a92071d20404dc623cfbaa4d
figaro27 <sweetfigaro27@gmail.com>
2019-05-18 20:14:50 -0300

    +1

---
+++ resources/assets/js/components/Modals/ComponentModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="modal-full">
+    <b-modal title="Edit Package" ref="modal" @ok.prevent="e => ok(e)">
+      <b-row>
+        <b-col>
+          <div
+            v-for="component in meal.components"
+            :key="component.id"
+            class=""
+          >
+            {{ component.title }}
+
+            <b-checkbox-group
+              :options="getOptions(component)"
+              :min="component.minimum"
+              :max="component.maximum"
+            >
+            </b-checkbox-group>
+          </div>
+        </b-col>
+      </b-row>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    meal: {
+      required: true
+    }
+  },
+  methods: {
+    ok() {}
+  }
+};
+</script>
+
+<style></style>
