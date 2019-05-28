a5a032eafce7b218649e7c8f733ed29c036c0820
figaro27 <sweetfigaro27@gmail.com>
2019-05-28 18:52:22 -0300

    default ingredient image

---
+++ database/migrations/2019_05_28_214255_add_default_image_to_ingredients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDefaultImageToIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ingredients', function (Blueprint $table) {
+            DB::statement(
+                "ALTER TABLE `ingredients` CHANGE `image` `image` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '/images/defaultIngredient.jpg', CHANGE `image_thumb` `image_thumb` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '\'/images/defaultIngredient.jpg';"
+            );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ingredients', function (Blueprint $table) {
+            DB::select(
+                "ALTER TABLE `ingredients` CHANGE `image` `image` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL, CHANGE `image_thumb` `image_thumb` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;"
+            );
+        });
+    }
+}
