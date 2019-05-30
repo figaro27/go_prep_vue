ede82d69347201cf3e7ea87debb8d89a6b14ac8f
figaro27 <sweetfigaro27@gmail.com>
2019-05-30 14:43:16 -0300

    +1

--- resources/assets/js/i18n/index.js
+++ resources/assets/js/i18n/index.js
@@ -23,7 +23,9 @@ const dateTimeFormats = {
 };
 
 // Create VueI18n instance with options
+import Vue from "vue";
 import VueI18n from "vue-i18n";
+Vue.use(VueI18n);
 const i18n = new VueI18n({
   locale: "en", // set locale
   messages,
