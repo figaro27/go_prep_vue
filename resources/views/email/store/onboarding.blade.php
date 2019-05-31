8607399550a04107b7b066b6bf434e31a22a1896
figaro27 <sweetfigaro27@gmail.com>
2019-05-31 17:12:24 -0400

    Ready

---
+++ resources/views/email/store/onboarding.blade.php
@@ -0,0 +1,173 @@
+                        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <!--[if !mso]><!-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!--<![endif]-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title></title>
+  <style type="text/css">
+.ReadMsgBody { width: 100%; background-color: #ffffff; }
+.ExternalClass { width: 100%; background-color: #ffffff; }
+.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div { line-height: 100%; }
+html { width: 100%; }
+body { -webkit-text-size-adjust: none; -ms-text-size-adjust: none; margin: 0; padding: 0; font-family: 'Open Sans', Arial, Sans-serif !important; }
+table { border-spacing: 0; table-layout: auto; margin: 0 auto; }
+img { display: block !important; overflow: hidden !important; }
+.yshortcuts a { border-bottom: none !important; }
+img:hover { opacity: 0.9 !important; }
+a { color: #4a4a4a; text-decoration: none; }
+.textbutton a { font-family: 'open sans', arial, sans-serif !important;}
+.btn-link a { color:#FFFFFF !important;}
+
+/*Responsive*/
+@media only screen and (max-width: 640px) {
+body { margin: 0px; width: auto !important; font-family: 'Open Sans', Arial, Sans-serif !important;}
+.table-inner { width: 90% !important;  max-width: 90%!important;}
+.table-full { width: 100%!important; max-width: 100%!important; text-align: center !important;}
+}
+
+@media only screen and (max-width: 479px) {
+body { width: auto !important; font-family: 'Open Sans', Arial, Sans-serif !important;}
+.table-inner{ width: 90% !important; text-align: center !important;}
+.table-full { width: 100%!important; max-width: 100%!important; text-align: center !important;}
+/*gmail*/
+u + .body .full { width:100% !important; width:100vw !important;}
+}
+</style>
+</head>
+
+<body class="body">
+  <!-- header -->
+  <table class="full" bgcolor="#f8f8f8" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+      <td align="center">
+        <table width="600" style="max-width: 600px" class="table-full" border="0" align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td align="center">
+              <table width="200" class="table-full" align="left" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td bgcolor="#3082CF" align="center">
+                    <table width="80%" class="table-inner" border="0" align="center" cellpadding="0" cellspacing="0">
+                      <tr>
+                        <td height="50"></td>
+                      </tr>
+                      <!-- logo -->
+                      <tr>
+                        <td align="center" style="line-height:0px;"><img style="display:block;font-size:0px; border:0px; line-height:0px;" src="https://goprep.com/logo.png" alt="GoPrep" title="GoPrep" /></td>
+                      </tr>
+                      <!-- end logo -->
+                      <tr>
+                        <td height="40"></td>
+                      </tr>
+                      <!-- company name -->
+                      <!-- end company name -->
+                      <tr>
+                        <td height="5"></td>
+                      </tr>
+                      <!-- address -->
+                      <tr>
+                        <td style="font-family: 'Open Sans', Arial, sans-serif; font-size:13px; color:#FFFFFF; line-height:26px;text-align: center;" align="center"> Meal Prep, Simplified.
+                           </td>
+                      </tr>
+                      <!-- end address -->
+                      <tr>
+                        <td height="80"></td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+              <!--[if (gte mso 9)|(IE)]></td><td><![endif]-->
+              <table width="400" class="table-full" border="0" align="right" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td align="center">
+                    <table width="90%" border="0" align="center" cellpadding="0" cellspacing="0">
+                      <tr>
+                        <td height="50"></td>
+                      </tr>
+                      <!-- title -->
+                      <tr>
+                        <td align="right" style="font-family: 'Open Sans', Arial, sans-serif; font-size:28px; color:#3b3b3b; line-height:26px;">Welcome Aboard!</td>
+                      </tr>
+                      <!-- end title -->
+                      <tr>
+                        <td height="25"></td>
+                      </tr>
+                      <tr>
+                        <td align="right">
+                          <table align="right" width="50" border="0" cellpadding="0" cellspacing="0">
+                            <tr>
+                              <td bgcolor="#E85A00" height="3" style="line-height:0px; font-size:0px;">&nbsp;</td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td height="15"></td>
+                      </tr>
+                      <!-- company name -->
+                      
+                      <tr>
+                        <td height="25">
+                          
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+  <table class="full" width="100%" align="center" bgcolor="#FFFFFF" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td align="center">
+        <table align="center" width="600" style="max-width:600px;" class="table-full" border="0" cellspacing="0" cellpadding="0">
+          <!-- header -->
+          <tr>
+            <td>
+              <table class="table-inner" width="100%" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                  <td width="263" align="left" valign="top" style="font-family: 'Open Sans', Arial, sans-serif; font-size:13px; color:#3b3b3b; line-height:26px;">
+                  <p>Hey {{ $user->userDetail->firstname }}!</p>
+                  <p>Welcome to GoPrep. We’re excited to have you as part of the team. Now that you are registered, there are just a few small steps remaining to get your store up and running. Our goal is to help you save time, money, and grow your meal prep operation.</p>
+
+                  <p>To open your store, all you need to complete is the following: </p>
+                  <ul>
+                  <li>Pick Your Delivery / Pickup Day(s)</li>
+                  <li>Set Your Cutoff Period</li>
+                  <li>Connect Your Bank Account to Stripe</li>
+                  <li>Add At Least One Meal On Your Menu Page</li>
+                  </ul>
+
+                  <p>Everything else is optional.</p>
+                  <p>We will happily do the entire setup for you for free. Just reply to this email or call us at (866) 5GO-PREP. The only things we’d need are the answers to a few quick questions, and a list of your menu items with prices.</p>
+
+                  <p>You can also view our extensive onboarding guide here:
+                  <a href="https://www.goprep.com/documentation">https://www.goprep.com/documentation</a></p>
+
+                  <p>Thanks again and we hope you enjoy your experience with GoPrep!</p>
+                </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <!-- end header -->
+          <tr>
+            <td height="10" style="border-bottom:3px solid #bcbcbc;"></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
+
+
