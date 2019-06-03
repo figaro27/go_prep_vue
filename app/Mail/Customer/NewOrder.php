661edb31c7904b5b2875bee6109f88ba4306dc53
figaro27 <sweetfigaro27@gmail.com>
2019-06-03 19:31:29 -0400

    +1

--- app/Mail/Customer/NewOrder.php
+++ app/Mail/Customer/NewOrder.php
@@ -30,10 +30,7 @@ class NewOrder extends Mailable
      */
     public function build()
     {
-        $email = $this->data
-            ->order()
-            ->with(['store', 'store.user'])
-            ->get()->store->user->email;
+        $email = $email = $this->data[0]->order->store->user->email;
         return $this->view('email.customer.new-order')
             ->with($this->data)
             ->subject('New Order')
