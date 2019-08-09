b345f9aa14786f758b3760fe30ac7a5f01800dc9
figaro27 <sweetfigaro27@gmail.com>
2019-08-09 15:04:05 -0400

    Finished

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>GoPrep</title>
+    @if ($store != null)
+    <title>{{ $store->details->name }} </title>
+    @else
+    <title>GoPrep</title>
+    @endif
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
