31078511ef083fc98ae0a120f5044fb2dc8bd533
figaro27 <sweetfigaro27@gmail.com>
2019-09-22 18:52:40 -0400

    +1

--- app/Meal.php
+++ app/Meal.php
@@ -102,7 +102,11 @@ class Meal extends Model implements HasMedia
 
     public function getMealSizeAttribute()
     {
-        if ($this->pivot && $this->pivot->meal_size_id) {
+        if (
+            $this->pivot &&
+            $this->pivot->meal_size_id &&
+            $this->pivot->meal_size
+        ) {
             return MealSize::find($this->pivot->meal_size_id);
         } else {
             return null;
@@ -114,7 +118,11 @@ class Meal extends Model implements HasMedia
         $title = $this->title;
 
         if ($this->has('sizes')) {
-            if ($this->pivot && $this->pivot->meal_size_id) {
+            if (
+                $this->pivot &&
+                $this->pivot->meal_size_id &&
+                $this->pivot->meal_size
+            ) {
                 return $title . ' - ' . $this->meal_size->title;
             } /*elseif ($this->default_size_title) {
         return $title . ' - ' . $this->default_size_title;
@@ -129,7 +137,11 @@ class Meal extends Model implements HasMedia
         $price = $this->price;
 
         if ($this->has('sizes')) {
-            if ($this->pivot && $this->pivot->meal_size_id) {
+            if (
+                $this->pivot &&
+                $this->pivot->meal_size_id &&
+                $this->pivot->meal_size
+            ) {
                 $price = $this->meal_size->price;
             }
         }
