526c611aeed243b80d466f73cb162cc111e40124
figaro27 <sweetfigaro27@gmail.com>
2019-09-30 13:47:02 -0400

    Update

--- resources/assets/js/components/Customer/CheckoutArea.vue
+++ resources/assets/js/components/Customer/CheckoutArea.vue
@@ -1111,9 +1111,11 @@ export default {
       }
 
       if (this.checkingOut) {
+        console.log("blocked...");
         return;
       }
 
+      console.log("calling...");
       this.checkingOut = true;
 
       this.deliveryFee = this.deliveryFeeAmount;
