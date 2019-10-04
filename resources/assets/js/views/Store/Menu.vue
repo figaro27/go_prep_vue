bd94fa07790a3fe02c2393301adc903b464bb7ad
figaro27 <sweetfigaro27@gmail.com>
2019-10-04 14:42:33 -0400

    Fixing view meal modal

--- resources/assets/js/views/Store/Menu.vue
+++ resources/assets/js/views/Store/Menu.vue
@@ -525,6 +525,7 @@
                         <i class="fa fa-trash"></i>
                       </b-btn>
                       <thumbnail
+                        v-if="image.url_thumb != null"
                         :src="image.url_thumb"
                         width="100%"
                       ></thumbnail>
@@ -551,7 +552,7 @@
           <b-col md="3" lg="2">
             <picture-input
               :ref="`featuredImageInput${meal.id}`"
-              :prefill="meal.image.url_thumb ? meal.image.url_thumb : false"
+              :prefill="getMealImage(meal)"
               @prefill="$refs[`featuredImageInput${meal.id}`].onResize()"
               :alertOnError="false"
               :autoToggleAspectRatio="true"
@@ -1548,6 +1549,10 @@ export default {
           this.$toastr.s("Category name updated.");
           location.reload();
         });
+    },
+    getMealImage(meal) {
+      if (meal.image === null) return null;
+      else return meal.image.url_thumb ? meal.image.url_thumb : false;
     }
   }
 };
