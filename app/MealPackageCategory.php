75e30db338b2572a18d5d9adff0584c1d7cdd8d2
figaro27 <sweetfigaro27@gmail.com>
2019-10-07 16:06:02 -0400

    removed migration

--- app/MealPackageCategory.php
+++ app/MealPackageCategory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class MealPackageCategory extends Pivot
 {
-    public $table = 'meal_package_category';
+    public $table = 'category_meal_package';
 
     public $fillable = ['meal_package_id', 'category_id'];
 
