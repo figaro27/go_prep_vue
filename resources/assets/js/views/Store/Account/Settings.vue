5d2800d0fa4ddeaa8009d94e0ba5947b69594b80
figaro27 <sweetfigaro27@gmail.com>
2019-10-20 22:31:44 -0400

    no message

--- resources/assets/js/views/Store/Account/Settings.vue
+++ resources/assets/js/views/Store/Account/Settings.vue
@@ -667,7 +667,7 @@
                   buttons
                   v-model="storeSettings.menuStyle"
                   class="storeFilters"
-                  @input.native="updateStoreSettings"
+                  @input="updateStoreSettings"
                   :options="[
                     { value: 'image', text: 'Image Based' },
                     { value: 'text', text: 'Text Based' }
