e845c4155e1968272f085799fc72732fb01ca9d5
figaro27 <sweetfigaro27@gmail.com>
2019-10-28 06:53:54 -0400

    Fixed style

--- resources/assets/js/views/Customer/Bag.vue
+++ resources/assets/js/views/Customer/Bag.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main-customer-container box-shadow top-fill">
+  <div class="main-customer-container menu-width box-shadow top-fill">
     <div class="bag">
       <auth-modal :showAuthModal="showAuthModal"></auth-modal>
       <spinner v-if="loading" position="absolute"></spinner>
