fcc060b5e66f0a32636045b2797c6164a805a9b6
figaro27 <sweetfigaro27@gmail.com>
2019-11-04 12:58:09 -0500

    no message

--- resources/views/reports/order_packing_slip_pdf.blade.php
+++ resources/views/reports/order_packing_slip_pdf.blade.php
@@ -57,7 +57,7 @@ $balance = $order->balance;
       <div class="col-md-12">
         <center>
         <div class="center-text">
-          <h4 class="center-text" style="text-transform: uppercase"><b>{{ $order->store->details->name }}</b></h4>
+          <h4 class="center-text bold-text" style="text-transform: uppercase"><b>{{ $order->store->details->name }}</b></h4>
           <img style="zoom: 1" src="{{$logo}}" />
         </div>
       </center>
