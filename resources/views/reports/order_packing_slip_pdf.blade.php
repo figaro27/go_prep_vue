e7a9a7b85a89918213a3dca2d3ced45fda21bc46
figaro27 <sweetfigaro27@gmail.com>
2019-11-04 17:00:37 -0500

    no message

--- resources/views/reports/order_packing_slip_pdf.blade.php
+++ resources/views/reports/order_packing_slip_pdf.blade.php
@@ -63,14 +63,14 @@ $balance = $order->balance;
 
   <div class="row">
       <div class="col-md-4">
-          <h6 class="bold-text" style="position:relative;top:30px">ORDER DATE {{$order->created_at->format('m/d/Y')}}</h6>
+          <h6 class="bold-text" style="position:relative;top:30px">{{$order->created_at->format('m/d/Y')}}</h6>
       </div>
       <div class="col-md-4 center-text">
           <h4 class="center-text bold-text" style="text-transform: uppercase"><b>{{ $order->store->details->name }}</b></h4>
           <img style="zoom: 1" src="{{$logo}}" />
       </div>
       <div class="col-md-4">
-          <h6 class="bold-text" style="position:relative;top:30px">ORDER #{{$order->order_number}}</h6>
+          <h6 class="bold-text" style="position:relative;top:30px">{{$order->order_number}}</h6>
       </div>
     </div>
 
