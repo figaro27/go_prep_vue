e18231b259c64a458375b97e514f81ffffe6745a
figaro27 <sweetfigaro27@gmail.com>
2019-11-05 13:54:48 -0500

    no message

--- resources/views/reports/order_packing_slip_pdf.blade.php
+++ resources/views/reports/order_packing_slip_pdf.blade.php
@@ -253,17 +253,17 @@ $brandColor = $order->store->settings->color;
           </table>
         </td>
       </tr>
-      <tfoot>
+      <tfoot style="-webkit-border-radius: 0px 0px 6px 6px;-moz-border-radius: 0px 0px 6px 6px;border-radius: 0px 0px 6px 6px;">
         @if ($order->balance > 0)
         <tr>
           <td style="border:none;font-size:18px"><b>Amount Due</b></td>
-          <td class="drop-shadow no-border" style="border:none;text-align:right;position:relative;right:20px;font-size:18px">${{number_format($order->balance, 2)}}</td>
+          <td style="border:none;text-align:right;position:relative;right:20px;font-size:18px">${{number_format($order->balance, 2)}}</td>
         </tr>
         @endif
         @if ($order->balance <= 0)
         <tr>
           <td style="border:none;font-size:18px"><b>Total Paid</b></td>
-          <td class="drop-shadow no-border" style="border:none;text-align:right;position:relative;right:20px;font-size:18px">{{ $amount }}</td>
+          <td style="border:none;text-align:right;position:relative;right:20px;font-size:18px">{{ $amount }}</td>
         </tr>
         @endif
       </tfoot>
