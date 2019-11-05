eb57168f7803656946547463a3b021b79987390c
figaro27 <sweetfigaro27@gmail.com>
2019-11-05 10:42:00 -0500

    no message

--- resources/views/reports/order_packing_slip_pdf.blade.php
+++ resources/views/reports/order_packing_slip_pdf.blade.php
@@ -37,7 +37,7 @@ $brandColor = '#'.$order->store->settings->color;
 
     table th {
       color: white;
-      background: @php echo $brandColor; @endphp;
+      background: @php echo $brandColor; @endphp !important;
     }
 
     table {
