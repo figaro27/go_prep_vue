1c47d5fb3f59d5f1d0281c8945d072edc680c4cc
figaro27 <sweetfigaro27@gmail.com>
2019-11-06 12:09:34 -0500

    no message

--- resources/views/reports/order_packing_slip_pdf.blade.php
+++ resources/views/reports/order_packing_slip_pdf.blade.php
@@ -255,18 +255,18 @@ $brandColor = $order->store->settings->color;
           </table>
         </td>
       </tr>
-      <tfoot class="drop-shadow">
+      <tfoot>
         @if ($order->balance > 0)
         <tr>
-          <th class="full-left-border-radius bold-text" style="border:none;font-size:18px;position:relative;left:30px">
+          <th class="full-left-border-radius bold-text drop-shadow" style="border:none;font-size:18px;position:relative;left:30px">
           Amount Due</th>
-          <th class="full-right-border-radius bold-text" style="border:none;font-size:18px;text-align:right;position:relative;right:20px">${{number_format($order->balance, 2)}}</th>
+          <th class="full-right-border-radius bold-text drop-shadow" style="border:none;font-size:18px;text-align:right;position:relative;right:20px">${{number_format($order->balance, 2)}}</th>
         </tr>
         @endif
         @if ($order->balance <= 0)
         <tr>
-          <th class="full-left-border-radius bold-text" style="border:none;font-size:18px;position:relative;left:30px"><b>Total Paid</b></th>
-          <th class="full-right-border-radius bold-text" style="border:none;font-size:18px;text-align:right;position:relative;right:20px">{{ $amount }}</th>
+          <th class="full-left-border-radius bold-text drop-shadow" style="border:none;font-size:18px;position:relative;left:30px"><b>Total Paid</b></th>
+          <th class="full-right-border-radius bold-text drop-shadow" style="border:none;font-size:18px;text-align:right;position:relative;right:20px">{{ $amount }}</th>
         </tr>
         @endif
       </tfoot>
