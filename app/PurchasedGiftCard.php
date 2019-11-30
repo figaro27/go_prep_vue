66bf345ce34f04bf05cfeb9a67be9c46dc94f97f
figaro27 <sweetfigaro27@gmail.com>
2019-11-30 14:54:30 -0500

    no message

--- app/PurchasedGiftCard.php
+++ app/PurchasedGiftCard.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App;
-
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class PurchasedGiftCard extends Model
 {
-    //
+    protected $appends = ['purchased_by'];
+
+    public function getPurchasedByAttribute()
+    {
+        $user = User::where('id', $this->user_id)->first();
+        return $user->details->firstname . ' ' . $user->details->lastname;
+    }
 }
