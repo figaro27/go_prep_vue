df6a84ca6506a9a817343371f9b37612a22d2dad
figaro27 <sweetfigaro27@gmail.com>
2019-12-05 17:54:17 -0500

    no message

--- app/MealOrder.php
+++ app/MealOrder.php
@@ -104,7 +104,7 @@ class MealOrder extends Pivot
 
         if ($this->special_instructions != null) {
             $title .=
-                '<p style="font-size:13px;font-weight:bold;">' .
+                '<p style="font-size:14px;font-weight:bold;">' .
                 $this->special_instructions .
                 '</p>';
         }
