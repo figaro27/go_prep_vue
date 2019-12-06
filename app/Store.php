9bc0a577f031003134952e31a09b2b563b1e5166
figaro27 <sweetfigaro27@gmail.com>
2019-12-05 22:26:54 -0500

    no message

--- app/Store.php
+++ app/Store.php
@@ -596,8 +596,8 @@ class Store extends Model
 
     public function deliversToZip($zip)
     {
-        $zip = strval($zip);
         foreach ($this->settings->delivery_distance_zipcodes as $zipcode) {
+            $zipcode = strval($zipcode);
             if (strpos($zip, $zipcode) !== false) {
                 return true;
             }
