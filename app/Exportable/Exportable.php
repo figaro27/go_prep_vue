cde1cda2aa93e423effef2e668d412af5461ce72
figaro27 <sweetfigaro27@gmail.com>
2019-12-07 15:11:48 -0500

    Update

--- app/Exportable/Exportable.php
+++ app/Exportable/Exportable.php
@@ -119,9 +119,6 @@ trait Exportable
 
         $html = view($this->exportPdfView(), $vars)->render();
 
-        var_dump($html);
-        exit();
-
         $pdfConfig = [
             'encoding' => 'utf-8',
             'orientation' => $this->orientation,
