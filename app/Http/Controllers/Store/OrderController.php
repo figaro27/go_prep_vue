00af99db1ca78077faa4b44642ff02a59f164e3e
figaro27 <sweetfigaro27@gmail.com>
2019-12-07 22:41:06 -0500

    no message

--- app/Http/Controllers/Store/OrderController.php
+++ app/Http/Controllers/Store/OrderController.php
@@ -118,6 +118,7 @@ class OrderController extends StoreController
                 ->with(['pickup_location'])
                 ->where(['paid' => 1])
                 ->where('delivery_date', '>=', $fromDate)
+                ->take(50)
                 ->get()
             : [];
 
