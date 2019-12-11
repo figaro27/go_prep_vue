aef9a562422020205f71650e89b5fd140eceac0c
figaro27 <sweetfigaro27@gmail.com>
2019-12-11 18:16:30 -0500

    no message

---
+++ database/migrations/2019_12_11_225156_add_macros_from_nutrition_column_to_store_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMacrosFromNutritionColumnToStoreSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('store_settings', function (Blueprint $table) {
+            $table
+                ->boolean('macrosFromNutrition')
+                ->after('showMacros')
+                ->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('store_settings', function (Blueprint $table) {
+            $table->dropColumn('macrosFromNutrition');
+        });
+    }
+}
