b972cc8e9494cccaa8aee6d98b6a0da5b1e65b74
figaro27 <sweetfigaro27@gmail.com>
2019-12-11 19:30:22 -0500

    no message

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -434,7 +434,7 @@ class CheckoutController extends UserController
                         'meal_id' => $item['meal']['id'],
                         'meal_size_id' => isset($item['size']['id'])
                             ? $item['size']['id']
-                            : null,
+                            : 10000,
                         'meal_package_id' => isset($item['meal_package_id'])
                             ? $item['meal_package_id']
                             : null,
