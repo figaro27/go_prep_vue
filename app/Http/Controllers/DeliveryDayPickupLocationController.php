95f37d826eee5a4f51166a55c719692a4ccc694f
figaro27 <sweetfigaro27@gmail.com>
2019-12-16 20:33:20 -0500

    Delivery Day Pickup Location pivot

---
+++ app/Http/Controllers/DeliveryDayPickupLocationController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DeliveryDayPickupLocation;
+use Illuminate\Http\Request;
+
+class DeliveryDayPickupLocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DeliveryDayPickupLocation  $deliveryDayPickupLocation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DeliveryDayPickupLocation $deliveryDayPickupLocation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DeliveryDayPickupLocation  $deliveryDayPickupLocation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DeliveryDayPickupLocation $deliveryDayPickupLocation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DeliveryDayPickupLocation  $deliveryDayPickupLocation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(
+        Request $request,
+        DeliveryDayPickupLocation $deliveryDayPickupLocation
+    ) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DeliveryDayPickupLocation  $deliveryDayPickupLocation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(
+        DeliveryDayPickupLocation $deliveryDayPickupLocation
+    ) {
+        //
+    }
+}
