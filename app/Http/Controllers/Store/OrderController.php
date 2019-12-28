0697eeeba3c19e7b0a48edb447d68a3ed881cb2e
figaro27 <sweetfigaro27@gmail.com>
2019-12-28 18:24:35 -0500

    no message

--- app/Http/Controllers/Store/OrderController.php
+++ app/Http/Controllers/Store/OrderController.php
@@ -538,6 +538,7 @@ class OrderController extends StoreController
             'payment_gateway',
             'paid',
             'paid_at',
+            'pickup_location_id',
             'purchasedGiftCardReduction',
             'purchased_gift_card_code',
             'purchased_gift_card_id',
