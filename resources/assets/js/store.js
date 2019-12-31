9c52c7d8dc7c7a05eb7ae0ac4705dada36bda9b9
figaro27 <sweetfigaro27@gmail.com>
2019-12-30 23:34:39 -0500

    no message

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -2623,7 +2623,6 @@ const getters = {
     }
   },
   viewedStoreMeal: state => (id, defaultMeal = null) => {
-    console.log(state.viewed_store.meals);
     try {
       let meal = null;
       if (defaultMeal != null) {
