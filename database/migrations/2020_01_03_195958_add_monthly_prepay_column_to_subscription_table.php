10d8c3f7205a09c350f7946b14b4b808cabc35d6
figaro27 <sweetfigaro27@gmail.com>
2020-01-03 15:20:40 -0500

    Stand alone subscriptions on meals & meal packages (basic version - to be reworked & finished properly in the future)

---
+++ database/migrations/2020_01_03_195958_add_monthly_prepay_column_to_subscription_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMonthlyPrepayColumnToSubscriptionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table
+                ->boolean('monthlyPrepay')
+                ->after('interval')
+                ->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->dropColumn('monthlyPrepay');
+        });
+    }
+}
