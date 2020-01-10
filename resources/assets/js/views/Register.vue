bd7e9f42103ce6352156e0599a38b6e846be9510
figaro27 <sweetfigaro27@gmail.com>
2020-01-10 18:05:44 -0500

    no message

--- resources/assets/js/views/Register.vue
+++ resources/assets/js/views/Register.vue
@@ -854,7 +854,7 @@ export default {
         data.user_details = { ...data.store };
       }
       if (
-        this.store &&
+        this.store.details &&
         this.store.details.country === "GB" &&
         !this.form[1].zip.includes(" ")
       ) {
