1b9604cf401dd6136cc81f8e08d926ae1e1411b3
figaro27 <sweetfigaro27@gmail.com>
2020-01-11 15:59:49 -0500

    Auto selecting pickup or delivery based on next orderable dates

--- resources/assets/js/components/Customer/CheckoutArea.vue
+++ resources/assets/js/components/Customer/CheckoutArea.vue
@@ -342,7 +342,7 @@
       <b-form-group>
         <b-form-radio-group v-model="pickup" v-on:input="changePickup">
           <b-form-radio
-            :value="0"
+            :value="pickupValue"
             v-if="
               storeSettings.next_orderable_delivery_dates.length > 0 ||
                 $route.params.storeView ||
@@ -352,7 +352,7 @@
             <strong>Delivery</strong>
           </b-form-radio>
           <b-form-radio
-            :value="1"
+            :value="deliveryValue"
             v-if="
               storeSettings.next_orderable_pickup_dates.length > 0 ||
                 $route.params.storeView ||
@@ -1080,6 +1080,26 @@ export default {
       bagDeliverySettings: "bagDeliverySettings",
       deliveryDays: "viewedStoreDeliveryDays"
     }),
+    deliveryValue() {
+      if (
+        this.storeSettings.next_orderable_delivery_dates.length === 0 &&
+        this.storeSettings.next_orderable_pickup_dates.length > 0
+      ) {
+        return 0;
+      } else {
+        return 1;
+      }
+    },
+    pickupValue() {
+      if (
+        this.storeSettings.next_orderable_delivery_dates.length === 0 &&
+        this.storeSettings.next_orderable_pickup_dates.length > 0
+      ) {
+        return 1;
+      } else {
+        return 0;
+      }
+    },
     pickupLocationClass() {
       if (this.selectedPickupLocationAddress && this.selectedPickupLocation)
         return "checkout-item h-90";
