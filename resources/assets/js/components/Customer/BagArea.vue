7cbaef279108dfeb201301b3fd9ac2742e8bfccf
figaro27 <sweetfigaro27@gmail.com>
2020-01-21 16:46:33 -0500

    no message

--- resources/assets/js/components/Customer/BagArea.vue
+++ resources/assets/js/components/Customer/BagArea.vue
@@ -909,7 +909,11 @@ export default {
       this.$emit("passPublicOrderNotes", this.publicOrderNotes);
     },
     setOrderLineItems(lineItemOrders) {
-      this.orderLineItems = this.$parent.order.line_items_order;
+      let extras = lineItemOrders;
+      this.orderLineItems = this.$route.params.adjustOrder
+        ? this.$parent.order.line_items_order
+        : extras;
+
       this.$emit("updateLineItems", this.orderLineItems);
     }
   }
