4fe0084e78fb3d6ff4a99ae6d2c2ce6dc91cd2f7
figaro27 <sweetfigaro27@gmail.com>
2020-01-27 14:16:01 -0500

    Adding idemptoency key to prevent duplciate charges

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -264,7 +264,13 @@ class CheckoutController extends UserController
                                 $afterDiscountBeforeFees * $application_fee
                             )
                         ],
-                        ["stripe_account" => $storeSettings->stripe_id]
+                        ["stripe_account" => $storeSettings->stripe_id],
+                        [
+                            "idempotency_key" =>
+                                substr(uniqid(rand(10, 99), false), 0, 14) .
+                                chr(rand(65, 90)) .
+                                rand(0, 9)
+                        ]
                     );
                 }
             } elseif ($gateway === Constants::GATEWAY_AUTHORIZE) {
