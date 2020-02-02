4daef4ace2c5929a4beab186496fcfab44ea6a0c
figaro27 <sweetfigaro27@gmail.com>
2020-02-01 21:37:12 -0300

    Create JS chunk for print

---
+++ resources/assets/js/print.js
@@ -0,0 +1,1 @@
+import nutritionFacts from "nutrition-label-jquery-plugin";
--- resources/views/reports/labels_pdf.blade.php
+++ resources/views/reports/labels_pdf.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <link rel="stylesheet" href="{{ asset(mix('/css/print.css')) }}">
-  <link ref="nutritionFacts" href="{{ asset('nutrition-label-jquery-plugin') }}">
+  <script src="{{ asset(mix('/js/print.js')) }}"></script>
 </head>
 
 <body class="{{ $body_classes }}">
