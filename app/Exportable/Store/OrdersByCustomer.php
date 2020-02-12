653fc1868309ba97290356569211ac65f225e877
figaro27 <sweetfigaro27@gmail.com>
2020-02-12 01:26:13 -0700

    Adding components & addons to excel order summary report

--- app/Exportable/Store/OrdersByCustomer.php
+++ app/Exportable/Store/OrdersByCustomer.php
@@ -154,8 +154,12 @@ class OrdersByCustomer
                     'meal_size' => $mealOrder->meal_size
                         ? $mealOrder->meal_size->title
                         : null,
-                    // 'meal_components' => $mealOrder->components ? $mealOrder->components : null,
-                    // 'meal_addons' => $mealOrder->addons ? $mealOrder->addons : null,
+                    'meal_components' => $mealOrder->components
+                        ? $mealOrder->componentsFormat
+                        : null,
+                    'meal_addons' => $mealOrder->addons
+                        ? $mealOrder->addonsFormat
+                        : null,
                     'quantity' => $mealOrder->quantity,
                     'price' =>
                         '$' .
@@ -177,8 +181,8 @@ class OrdersByCustomer
                 'Delivery Instructions',
                 'Meal',
                 'Size',
-                // 'Components',
-                // 'Addons',
+                'Components',
+                'Addons',
                 'Quantity',
                 'Price'
             ]);
