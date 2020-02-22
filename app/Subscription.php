7db867333fe6d61ef993f8ed79caef6fa73dbf6d
figaro27 <sweetfigaro27@gmail.com>
2020-02-22 02:21:45 -0500

    no message

--- app/Subscription.php
+++ app/Subscription.php
@@ -881,6 +881,9 @@ class Subscription extends Model
             $order->save();
 
             // Replace order meals
+            foreach ($order->meal_orders->addons as $addon) {
+                $addon->delete();
+            }
             $order->meal_orders()->delete();
             foreach ($bag->getItems() as $item) {
                 $mealOrder = new MealOrder();
