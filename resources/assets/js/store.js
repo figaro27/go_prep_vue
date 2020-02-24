7c9b10a37c9c665c217dd5d34424df8acfeecbb4
figaro27 <sweetfigaro27@gmail.com>
2020-02-23 20:19:28 -0500

    no message

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -521,31 +521,30 @@ const mutations = {
         let component = _.find(item.meal.components, {
           id: parseInt(componentId)
         });
-        if (component) {
-          if (!item.meal_package) {
-            _.forEach(choices, optionId => {
-              let option = _.find(component.options, {
-                id: parseInt(optionId)
-              });
-              price += option.price;
-            });
-          } else {
-            if (component.price) {
-              price += component.price;
-            }
-            _.forEach(choices, (choices, optionId) => {
-              let option = _.find(component.options, {
-                id: parseInt(optionId)
-              });
-              price += option.price;
 
-              _.forEach(choices, choice => {
-                if (choice.price) {
-                  price += choice.price;
-                }
-              });
+        if (!item.meal_package) {
+          _.forEach(choices, optionId => {
+            let option = _.find(component.options, {
+              id: parseInt(optionId)
             });
+            price += option.price;
+          });
+        } else {
+          if (component.price) {
+            price += component.price;
           }
+          _.forEach(choices, (choices, optionId) => {
+            let option = _.find(component.options, {
+              id: parseInt(optionId)
+            });
+            price += option.price;
+
+            _.forEach(choices, choice => {
+              if (choice.price) {
+                price += choice.price;
+              }
+            });
+          });
         }
       });
     } // End If
