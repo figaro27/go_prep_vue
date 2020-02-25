955535c99a1a6f0101607838ba118c4f1fb5ef76
figaro27 <sweetfigaro27@gmail.com>
2020-02-25 16:17:46 -0500

    no message

--- resources/assets/js/views/Store/Menu.vue
+++ resources/assets/js/views/Store/Menu.vue
@@ -1940,7 +1940,7 @@ export default {
       this.refreshTable();
       this.transferVariations = false;
       this.replaceVariations = false;
-      this.substituteMeal = null;
+      this.substituteMeal = "";
       this.substituteMealSizes = {};
       this.substituteMealComponents = {};
       this.substituteMealComponentOptions = {};
