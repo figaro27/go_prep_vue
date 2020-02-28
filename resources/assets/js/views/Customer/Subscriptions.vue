9cad0f7624d3db9522b4ebccb9efd0e8eef88baf
figaro27 <sweetfigaro27@gmail.com>
2020-02-27 20:42:30 -0500

    no message

--- resources/assets/js/views/Customer/Subscriptions.vue
+++ resources/assets/js/views/Customer/Subscriptions.vue
@@ -156,9 +156,7 @@ f<template>
             <span v-if="!storeModules.hideTransferOptions" class="mt-2">
               <h4 v-if="!subscription.pickup">Delivery Day</h4>
               <h4 v-if="subscription.pickup">Pickup Day</h4>
-              {{
-                {{ moment(subscription.next_renewal_at).format("dddd") }}
-              }}
+              {{ moment(props.row.next_renewal_at).format("dddd") }}
               <span v-if="subscription.transferTime">
                 {{ subscription.transferTime }}</span
               >
