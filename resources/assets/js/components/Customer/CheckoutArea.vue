eb6cbb0c50d26ca119268647920c5715d38b64f0
figaro27 <sweetfigaro27@gmail.com>
2020-03-16 12:38:16 -0400

    no message

--- resources/assets/js/components/Customer/CheckoutArea.vue
+++ resources/assets/js/components/Customer/CheckoutArea.vue
@@ -822,7 +822,7 @@
           >ADJUST ORDER</b-btn
         >
 
-        <div v-if="subscriptionId" class="d-none d-lg-block">
+        <div v-if="subscriptionId">
           <b-btn
             class="menu-bag-btn update-meals-btn"
             @click="updateSubscriptionMeals"
