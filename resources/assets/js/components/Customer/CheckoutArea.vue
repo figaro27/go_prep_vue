a87b0694c1234893603a41e538ccb40145a4f0ff
figaro27 <sweetfigaro27@gmail.com>
2020-05-28 10:24:49 -0400

    no message

--- resources/assets/js/components/Customer/CheckoutArea.vue
+++ resources/assets/js/components/Customer/CheckoutArea.vue
@@ -41,11 +41,21 @@
                 )
               }}
               more to receive
-              <span v-if="promotion.promotionType === 'flat'">{{
-                format.money(promotion.promotionAmount, storeSettings.currency)
-              }}</span>
-              <span v-else>{{ promotion.promotionAmount }}%</span>
-              off your order.
+              <span v-if="promotion.promotionAmount > 0">
+                <span v-if="promotion.promotionType === 'flat'">{{
+                  format.money(
+                    promotion.promotionAmount,
+                    storeSettings.currency
+                  )
+                }}</span>
+                <span v-else>{{ promotion.promotionAmount }}%</span>
+                off your order.
+              </span>
+              <span
+                v-if="promotion.promotionAmount === 0 && promotion.freeDelivery"
+              >
+                free delivery.
+              </span>
             </h6>
           </b-alert>
           <b-alert
