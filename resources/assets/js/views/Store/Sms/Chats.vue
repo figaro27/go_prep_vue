68e766760cd25778947fb85f973bb05d56f07e48
figaro27 <sweetfigaro27@gmail.com>
2020-06-12 21:49:00 -0400

    no message

--- resources/assets/js/views/Store/Sms/Chats.vue
+++ resources/assets/js/views/Store/Sms/Chats.vue
@@ -156,7 +156,7 @@ export default {
             text.innerCSS += "last";
           }
         });
-
+        this.conflict = false;
         // Refresh chats
         this.refreshSMSChats();
       });
