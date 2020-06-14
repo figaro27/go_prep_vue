8833354a7469d5ffb20291c7b1deb17e408b5173
figaro27 <sweetfigaro27@gmail.com>
2020-06-14 01:40:18 -0400

    no message

--- resources/assets/js/views/Store/Sms/Lists.vue
+++ resources/assets/js/views/Store/Sms/Lists.vue
@@ -78,14 +78,14 @@
       </div>
       <div slot="actions" class="text-nowrap" slot-scope="props">
         <button
-          v-if="!isAllContactsList(props.row.id)"
+          :disabled="!isAllContactsList(props.row.id)"
           class="btn view btn-warning btn-sm"
           @click="(list = props.row), (showEditListModal = true)"
         >
           Edit
         </button>
         <button
-          v-if="!isAllContactsList(props.row.id)"
+          disabled="!isAllContactsList(props.row.id)"
           class="btn btn-danger btn-sm"
           @click="deleteList(props.row.id)"
         >
