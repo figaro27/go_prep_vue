1848e8cb84bad842f02fef3e8672901fd84bf46e
figaro27 <sweetfigaro27@gmail.com>
2020-06-14 01:37:51 -0400

    no message

--- resources/assets/js/views/Store/Sms/Lists.vue
+++ resources/assets/js/views/Store/Sms/Lists.vue
@@ -1,100 +1,98 @@
 <template>
-  <div class="row mt-2">
-    <div class="col-md-12">
-      <div>
-        <img
-          v-b-popover.rightbottom.hover="
-            'A list is a group of contacts to make it easier to choose recipients when you send a new message. By default you will have a master list which is made up of all your past customers.'
-          "
-          title="Lists"
-          src="/images/store/popover.png"
-          class="popover-size mb-3"
-        />
-      </div>
-      <Spinner v-if="isLoading" />
-      <b-modal
-        size="lg"
-        title="Create List"
-        v-model="showCreateListModal"
-        v-if="showCreateListModal"
-        no-fade
-        hide-footer
-      >
-        <create-list @addList="addList($event)"></create-list>
-      </b-modal>
-
-      <b-modal
-        size="md"
-        title="Edit List"
-        v-model="showEditListModal"
-        v-if="showEditListModal"
-        no-fade
-        hide-footer
-      >
-        <edit-list
-          :list="list"
-          :tableData="contacts"
-          @updateList="updateList($event)"
-        ></edit-list>
-      </b-modal>
-
-      <b-modal
-        size="md"
-        title="Delete List"
-        v-model="showDeleteListModal"
-        v-if="showDeleteListModal"
-        no-fade
-        hide-footer
-      >
-        <delete-list
-          @cancel="showDeleteListModal = false"
-          @deleteList="destroy($event)"
-        ></delete-list>
-      </b-modal>
-
-      <button
-        class="btn btn-success btn-md mb-2 mb-sm-0"
-        @click="showCreateListModal = true"
-      >
-        Add New List
-      </button>
-
-      <v-client-table
-        v-show="initialized"
-        :columns="columns"
-        :data="SMSListsData"
-        :options="{
-          orderBy: {
-            column: 'created_at',
-            ascending: true
-          },
-          headings: {
-            membersCount: 'Contacts'
-          },
-          filterable: false
-        }"
-      >
-        <div slot="content" class="text-nowrap" slot-scope="props">
-          {{ truncate(props.row.content, 150, "...") }}
-        </div>
-        <div slot="actions" class="text-nowrap" slot-scope="props">
-          <button
-            v-if="!isAllContactsList(props.row.id)"
-            class="btn view btn-warning btn-sm"
-            @click="(list = props.row), (showEditListModal = true)"
-          >
-            Edit
-          </button>
-          <button
-            v-if="!isAllContactsList(props.row.id)"
-            class="btn btn-danger btn-sm"
-            @click="deleteList(props.row.id)"
-          >
-            Delete
-          </button>
-        </div>
-      </v-client-table>
+  <div>
+    <div>
+      <img
+        v-b-popover.rightbottom.hover="
+          'A list is a group of contacts to make it easier to choose recipients when you send a new message. By default you will have a master list which is made up of all your past customers.'
+        "
+        title="Lists"
+        src="/images/store/popover.png"
+        class="popover-size mb-3"
+      />
     </div>
+    <Spinner v-if="isLoading" />
+    <b-modal
+      size="lg"
+      title="Create List"
+      v-model="showCreateListModal"
+      v-if="showCreateListModal"
+      no-fade
+      hide-footer
+    >
+      <create-list @addList="addList($event)"></create-list>
+    </b-modal>
+
+    <b-modal
+      size="md"
+      title="Edit List"
+      v-model="showEditListModal"
+      v-if="showEditListModal"
+      no-fade
+      hide-footer
+    >
+      <edit-list
+        :list="list"
+        :tableData="contacts"
+        @updateList="updateList($event)"
+      ></edit-list>
+    </b-modal>
+
+    <b-modal
+      size="md"
+      title="Delete List"
+      v-model="showDeleteListModal"
+      v-if="showDeleteListModal"
+      no-fade
+      hide-footer
+    >
+      <delete-list
+        @cancel="showDeleteListModal = false"
+        @deleteList="destroy($event)"
+      ></delete-list>
+    </b-modal>
+
+    <button
+      class="btn btn-success btn-md mb-2 mb-sm-0"
+      @click="showCreateListModal = true"
+    >
+      Add New List
+    </button>
+
+    <v-client-table
+      v-show="initialized"
+      :columns="columns"
+      :data="SMSListsData"
+      :options="{
+        orderBy: {
+          column: 'created_at',
+          ascending: true
+        },
+        headings: {
+          membersCount: 'Contacts'
+        },
+        filterable: false
+      }"
+    >
+      <div slot="content" class="text-nowrap" slot-scope="props">
+        {{ truncate(props.row.content, 150, "...") }}
+      </div>
+      <div slot="actions" class="text-nowrap" slot-scope="props">
+        <button
+          v-if="!isAllContactsList(props.row.id)"
+          class="btn view btn-warning btn-sm"
+          @click="(list = props.row), (showEditListModal = true)"
+        >
+          Edit
+        </button>
+        <button
+          v-if="!isAllContactsList(props.row.id)"
+          class="btn btn-danger btn-sm"
+          @click="deleteList(props.row.id)"
+        >
+          Delete
+        </button>
+      </div>
+    </v-client-table>
   </div>
 </template>
 
