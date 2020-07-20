4bbca8ff00c781df63d6cefca3521f32171539c9
figaro27 <sweetfigaro27@gmail.com>
2020-07-20 15:58:37 -0400

    Nutritionix swap

--- config/nutritionix.php
+++ config/nutritionix.php
@@ -1,8 +1,17 @@
 <?php
 
+// return [
+//     'app_id' => 'd9ff48c3',
+//     'app_key' => '0ba7e0ffd7b498eaa01035b05fa8717f',
+//     'track_url' => 'https://trackapi.nutritionix.com/v2/',
+//     'nutrients_url' => 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+//     'item_search_url' => 'https://trackapi.nutritionix.com/v2/search/item',
+//     'search_url' => 'https://trackapi.nutritionix.com/v2/search/instant'
+// ];
+
 return [
-    'app_id' => 'd9ff48c3',
-    'app_key' => '0ba7e0ffd7b498eaa01035b05fa8717f',
+    'app_id' => '1cb5966f',
+    'app_key' => '95534f28cd549764a32c5363f13699a9',
     'track_url' => 'https://trackapi.nutritionix.com/v2/',
     'nutrients_url' => 'https://trackapi.nutritionix.com/v2/natural/nutrients',
     'item_search_url' => 'https://trackapi.nutritionix.com/v2/search/item',
