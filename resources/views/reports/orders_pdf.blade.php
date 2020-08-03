a0679da34dfe892c4fcfe6c7c815f4c3a360e05a
figaro27 <sweetfigaro27@gmail.com>
2020-08-02 23:21:43 -0400

    Showing Livotis regular orders report until they specificy the particular columns again

--- resources/views/reports/orders_pdf.blade.php
+++ resources/views/reports/orders_pdf.blade.php
@@ -33,9 +33,10 @@
             <th>Zip</th>
             <th>Phone</th>
             <th style="width:100px">Email</th>
-            @if (!isset($params['livotis']))
+            <!-- @if (!isset($params['livotis'])) -->
             <th>Total</th>
             <th>Balance</th>
+            <!-- @endif -->
             <th style="width:100px">Order Placed</th>
             <th style="width:100px">Delivery Date</th>
             @if ($params['show_times'])
