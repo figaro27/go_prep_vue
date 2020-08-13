cfc653582375e4706492b73d79b37956795fb7d1
figaro27 <sweetfigaro27@gmail.com>
2020-08-13 12:56:51 -0400

    no message
    (cherry picked from commit bed0acb1fd1494cf6a94e38846c68b079d7092e2)

--- app/Subscription.php
+++ app/Subscription.php
@@ -1105,10 +1105,13 @@ class Subscription extends Model
 
                 if (isset($item['addons']) && $item['addons']) {
                     foreach ($item['addons'] as $addon) {
-                        MealOrderAddon::create([
-                            'meal_order_id' => $mealOrder->id,
-                            'meal_addon_id' => $addon->addon->id
-                        ]);
+                        try {
+                            MealOrderAddon::create([
+                                'meal_order_id' => $mealOrder->id,
+                                'meal_addon_id' => $addon->addon->id
+                            ]);
+                        } catch (\Exception $e) {
+                        }
                     }
                 }
             }
