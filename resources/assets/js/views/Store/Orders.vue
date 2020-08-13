b877cf409b62f55c191cb6e349a7a12859b291f5
figaro27 <sweetfigaro27@gmail.com>
2020-08-13 11:37:07 -0400

    no message

--- resources/assets/js/views/Store/Orders.vue
+++ resources/assets/js/views/Store/Orders.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row mt-3">
     <div class="col-md-12">
-      <b-alert style="background-color:#EBFAFF" show dismissible>
+      <!-- <b-alert style="background-color:#EBFAFF" show dismissible>
         <h5>
           New Feature - Cooler Bag Deposit
         </h5>
@@ -17,7 +17,7 @@
             ></router-link
           >
         </p>
-      </b-alert>
+      </b-alert> -->
       <div class="card">
         <div class="card-body">
           <Spinner v-if="orders.loading" />
