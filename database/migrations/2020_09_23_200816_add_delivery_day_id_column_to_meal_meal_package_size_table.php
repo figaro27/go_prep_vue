dda9390df79c0c7fe1157079aa7fb0c651c8b5cf
figaro27 <sweetfigaro27@gmail.com>
2020-09-23 16:35:18 -0400

    no message

---
+++ database/migrations/2020_09_23_200816_add_delivery_day_id_column_to_meal_meal_package_size_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDeliveryDayIdColumnToMealMealPackageSizeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('meal_meal_package_size', function (Blueprint $table) {
+            $table
+                ->unsignedInteger('delivery_day_id')
+                ->references('id')
+                ->on('delivery_days')
+                ->after('quantity')
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('meal_meal_package_size', function (Blueprint $table) {
+            $table->dropColumn('delivery_day_id');
+        });
+    }
+}
