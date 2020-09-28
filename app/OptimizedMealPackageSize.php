02b5fd59cff055cc1b3349255b75baf8bd857d5c
figaro27 <sweetfigaro27@gmail.com>
2020-09-28 18:15:36 -0400

    Hiding deleted package sizes on menu

--- app/OptimizedMealPackageSize.php
+++ app/OptimizedMealPackageSize.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OptimizedMealPackageSize extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'meal_package_sizes';
 
     public $fillable = [];
