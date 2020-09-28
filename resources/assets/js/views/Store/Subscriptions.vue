22ae83aa2cbe9240eb7ef90ee1e8f9d125db9f5f
figaro27 <sweetfigaro27@gmail.com>
2020-09-28 18:04:17 -0400

    no message

--- resources/assets/js/views/Store/Subscriptions.vue
+++ resources/assets/js/views/Store/Subscriptions.vue
@@ -655,7 +655,7 @@ export default {
           subtotal: format.money(item.price, subscription.currency)
         };
       });
-      data = _.orderBy(data, "delivery_date");
+      // data = _.orderBy(data, "delivery_date");
       return _.filter(data);
     },
     viewSubscription(id) {
