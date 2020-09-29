6b94e94206b007c37b41f594bed80015ac6d34c6
figaro27 <sweetfigaro27@gmail.com>
2020-09-29 16:42:55 -0400

    Revert "Revert "If meal or package doesn't have any assigned delivery days, show on all delivery days""
    This reverts commit dfcacc1454caf3bcf0448b323cbd7f746ed4316e.

--- app/Http/Controllers/SpaController.php
+++ app/Http/Controllers/SpaController.php
@@ -682,14 +682,16 @@ class SpaController extends Controller
                         ]);
 
                     if ($delivery_day_id != 0 && $isValidDD) {
-                        $meals = $meals->whereHas('days', function (
-                            $query
-                        ) use ($delivery_day_id) {
-                            $query->where(
-                                'delivery_day_meals.delivery_day_id',
+                        $meals = $meals
+                            ->doesntHave('days')
+                            ->orWhereHas('days', function ($query) use (
                                 $delivery_day_id
-                            );
-                        });
+                            ) {
+                                $query->where(
+                                    'delivery_day_meals.delivery_day_id',
+                                    $delivery_day_id
+                                );
+                            });
                     }
 
                     $meals = $meals
@@ -718,14 +720,16 @@ class SpaController extends Controller
                         ]);
 
                     if ($delivery_day_id != 0 && $isValidDD) {
-                        $packages = $packages->whereHas('days', function (
-                            $query
-                        ) use ($delivery_day_id) {
-                            $query->where(
-                                'delivery_day_meal_packages.delivery_day_id',
+                        $packages = $packages
+                            ->doesntHave('days')
+                            ->orWhereHas('days', function ($query) use (
                                 $delivery_day_id
-                            );
-                        });
+                            ) {
+                                $query->where(
+                                    'delivery_day_meal_packages.delivery_day_id',
+                                    $delivery_day_id
+                                );
+                            });
                     }
 
                     $packages = $packages
