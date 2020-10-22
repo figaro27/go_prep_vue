f2b223482a43c761d911dcdeae01735f45fab15a
figaro27 <sweetfigaro27@gmail.com>
2020-10-22 13:13:45 -0400

    no message

--- app/Meal.php
+++ app/Meal.php
@@ -935,7 +935,7 @@ class Meal extends Model implements HasMedia
 
                     $mealComponent->title = $component['title'];
                     $mealComponent->minimum = isset($component['minimum'])
-                        ? $comonent['minimum']
+                        ? $component['minimum']
                         : 1;
                     $mealComponent->maximum = isset($component['maximum'])
                         ? $component['maximum']
@@ -1306,7 +1306,7 @@ class Meal extends Model implements HasMedia
 
                 $mealComponent->title = $component['title'];
                 $mealComponent->minimum = isset($component['minimum'])
-                    ? $comonent['minimum']
+                    ? $component['minimum']
                     : 1;
                 $mealComponent->maximum = isset($component['maximum'])
                     ? $component['maximum']
