16ed908f2355ac563476a906102569348393e8ec
figaro27 <sweetfigaro27@gmail.com>
2020-11-14 23:30:08 -0500

    no message

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,9 @@
     <title>GoPrep</title>
     @endif
     @php
-    @if ($store != null)
+    if ($store != null){
     $faviconUrl = $store->menuSettings->favicon;
-    @endif
+    }
     @endphp
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
