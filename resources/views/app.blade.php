bdf7d1eeaab92851556be2e126bd9375ea5b5f7c
figaro27 <sweetfigaro27@gmail.com>
2020-11-14 23:27:29 -0500

    no message

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,10 +6,11 @@
     @else
     <title>GoPrep</title>
     @endif
+    @php
     @if ($store != null)
     $faviconUrl = $store->menuSettings->favicon;
     @endif
-
+    @endphp
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
