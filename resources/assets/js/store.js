a5d001ac7771ef0302a764c27c247520d23c77d0
figaro27 <sweetfigaro27@gmail.com>
2020-11-23 01:53:43 -0500

    no message

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -658,26 +658,17 @@ const mutations = {
       } else {
         _.forEach(item.addons, (choices, addonId) => {
           let addon = _.find(item.meal.addons, { id: parseInt(addonId) });
-
-          if (
-            !choices.some(choice => {
-              return choice.price > 0;
-            })
-          ) {
-            price += addon ? addon.price : 0;
-          } else {
-            // Add base addon price * choices selected
-            if (addon.price) {
-              price += addon ? addon.price * Math.max(1, choices.length) : 0;
-            }
-
-            // Add addon choice prices
-            _.forEach(choices, choice => {
-              if (choice.price) {
-                price += choice ? choice.price : 0;
-              }
-            });
+          // Add base addon price * choices selected
+          if (addon.price) {
+            price += addon ? addon.price * Math.max(1, choices.length) : 0;
           }
+
+          // Add addon choice prices
+          _.forEach(choices, choice => {
+            if (choice.price) {
+              price += choice ? choice.price : 0;
+            }
+          });
         });
       }
     } // End IF
