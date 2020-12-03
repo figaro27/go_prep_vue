1aa3cdd6bd3ad5bd06ed9af227ace3caa61b8bc9
figaro27 <sweetfigaro27@gmail.com>
2020-12-03 13:00:19 -0500

    no message

--- config/nutritionix.php
+++ config/nutritionix.php
@@ -1,13 +1,13 @@
 <?php
 
-return [
-    'app_id' => 'd9ff48c3',
-    'app_key' => '0ba7e0ffd7b498eaa01035b05fa8717f',
-    'track_url' => 'https://trackapi.nutritionix.com/v2/',
-    'nutrients_url' => 'https://trackapi.nutritionix.com/v2/natural/nutrients',
-    'item_search_url' => 'https://trackapi.nutritionix.com/v2/search/item',
-    'search_url' => 'https://trackapi.nutritionix.com/v2/search/instant'
-];
+// return [
+//     'app_id' => 'd9ff48c3',
+//     'app_key' => '0ba7e0ffd7b498eaa01035b05fa8717f',
+//     'track_url' => 'https://trackapi.nutritionix.com/v2/',
+//     'nutrients_url' => 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+//     'item_search_url' => 'https://trackapi.nutritionix.com/v2/search/item',
+//     'search_url' => 'https://trackapi.nutritionix.com/v2/search/instant'
+// ];
 
 return [
     'app_id' => '1cb5966f',
