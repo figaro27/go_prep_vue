713c09538cc76de84d8b51c58a5badb4bdff69df
figaro27 <sweetfigaro27@gmail.com>
2020-12-21 18:52:02 -0500

    Reworking design of meal filter modal

--- resources/assets/js/components/Customer/MealFilterModal.vue
+++ resources/assets/js/components/Customer/MealFilterModal.vue
@@ -1,8 +1,82 @@
 <template>
   <div>
     <div class="modal-basic">
-      <b-modal size="md" v-model="view" @hide="hide" no-fade centered>
-        <div>
+      <b-modal
+        size="md"
+        v-model="view"
+        no-fade
+        centered
+        hide-header
+        hide-footer
+      >
+        <div class="d-flex" style="justify-content:space-around">
+          <div class="mt-3">
+            <h3>Tags</h3>
+            <p class="font-14">Include Meals That Are:</p>
+            <div
+              v-for="tag in tags"
+              :key="`tag-${tag}`"
+              class="filters"
+              style="margin-top: 5px; margin-bottom: 5px;"
+            >
+              <span
+                class="badge badge-pill badge-light"
+                @click="$emit('filterByTag', tag)"
+                v-if="!$parent.active[tag]"
+              >
+                {{ tag }}
+              </span>
+              <span
+                class="badge badge-pill brand-color white-text"
+                @click="$emit('filterByTag', tag)"
+                v-if="$parent.active[tag]"
+              >
+                {{ tag }}
+              </span>
+            </div>
+          </div>
+          <div class="mt-3">
+            <h3>Allergies</h3>
+            <p class="font-14">Hide Meals That Contain:</p>
+            <div
+              v-for="allergy in allergies"
+              :key="`allergy-${allergy.id}`"
+              class="filters"
+              style="margin-top: 5px; margin-bottom: 5px;"
+            >
+              <span
+                class="badge badge-pill badge-light"
+                @click="$parent.filterByAllergy(allergy.id)"
+                v-if="!$parent.active[allergy.id]"
+              >
+                {{ allergy.title }}
+              </span>
+              <span
+                class="badge badge-pill badge-primary"
+                @click="$parent.filterByAllergy(allergy.id)"
+                v-if="$parent.active[allergy.id]"
+              >
+                {{ allergy.title }}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex mt-3" style="justify-content:center">
+          <div>
+            <b-btn
+              @click="$parent.viewFilterModalParent = false"
+              variant="secondary"
+              class="mt-2"
+              >Back</b-btn
+            >
+            <b-btn
+              @click="$emit('clearFilters')"
+              class="brand-color white-text mt-2 ml-2"
+              >Clear All</b-btn
+            >
+          </div>
+        </div>
+        <!-- <div>
           <h4 class="center-text mt-4">Allergies</h4>
           <p class="center-text">Hide Items That Contain:</p>
         </div>
@@ -47,7 +121,7 @@
           @click="$emit('clearFilters')"
           class="center mt-4 brand-color white-text"
           >Clear All</b-button
-        >
+        > -->
       </b-modal>
     </div>
   </div>
