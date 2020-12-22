4923a43dabe90043bc949375dfe40af4034521e0
figaro27 <sweetfigaro27@gmail.com>
2020-12-21 19:03:11 -0500

    no message

--- resources/assets/js/components/Customer/MealFilterModal.vue
+++ resources/assets/js/components/Customer/MealFilterModal.vue
@@ -3,11 +3,12 @@
     <div class="modal-basic">
       <b-modal
         size="md"
-        v-model="view"
+        v-model="$parent.viewFilterModalParent"
         no-fade
         centered
         hide-header
         hide-footer
+        @hide="hide"
       >
         <div class="d-flex" style="justify-content:space-around">
           <div class="mt-3">
@@ -76,52 +77,6 @@
             >
           </div>
         </div>
-        <!-- <div>
-          <h4 class="center-text mt-4">Allergies</h4>
-          <p class="center-text">Hide Items That Contain:</p>
-        </div>
-        <div
-          class="mb-4"
-          style="display: flex; flex-wrap: wrap; justify-content: center;"
-        >
-          <div
-            v-for="allergy in allergies"
-            :key="`allergy-${allergy.id}`"
-            class="filters"
-            style="margin-top: 5px; margin-bottom: 5px;"
-          >
-            <b-button
-              :pressed="$parent.active[allergy.id]"
-              @click="$parent.filterByAllergy(allergy.id)"
-              >{{ allergy.title }}</b-button
-            >
-          </div>
-        </div>
-        <hr />
-        <div>
-          <h4 class="center-text mt-5">Nutrition</h4>
-          <p class="center-text">Show Items That Are:</p>
-        </div>
-        <div style="display: flex; flex-wrap: wrap; justify-content: center;">
-          <div
-            v-for="tag in tags"
-            :key="`tag-${tag}`"
-            class="filters"
-            style="margin-top: 5px; margin-bottom: 5px;"
-          >
-            <b-button
-              :pressed="$parent.active[tag]"
-              @click="$emit('filterByTag', tag)"
-            >
-              {{ tag }}
-            </b-button>
-          </div>
-        </div>
-        <b-button
-          @click="$emit('clearFilters')"
-          class="center mt-4 brand-color white-text"
-          >Clear All</b-button
-        > -->
       </b-modal>
     </div>
   </div>
