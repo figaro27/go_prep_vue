2c4ed70735ce50595e50618101ea74b9dcd7a3ea
figaro27 <sweetfigaro27@gmail.com>
2021-01-14 18:43:21 -0500

    no message

--- resources/assets/js/components/Customer/RegisterGuest.vue
+++ resources/assets/js/components/Customer/RegisterGuest.vue
@@ -134,10 +134,7 @@
       </b-form-group>
       <p class="font-italic">
         By checking out you agree to our
-        <span
-          class="strong"
-          @click.stop.prevent="$refs.tos.show()"
-          @touch.stop.prevent="$refs.tos.show()"
+        <span class="strong" @click="$refs.tos.show()" @touch="$refs.tos.show()"
           >terms of service.</span
         >
       </p>
