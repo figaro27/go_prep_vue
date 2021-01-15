4f9a04b61059e93a7f57787ccb116cec3e97484b
figaro27 <sweetfigaro27@gmail.com>
2021-01-15 13:27:10 -0500

    no message

--- resources/assets/js/components/Customer/RegisterGuest.vue
+++ resources/assets/js/components/Customer/RegisterGuest.vue
@@ -282,16 +282,16 @@ export default {
     this.form[0].password = this.uniqid();
     this.form[0].password_confirmation = this.form[0].password;
 
-    if (this.store.details) {
-      this.form[1].state = this.store.details.state;
-      this.form[1].country = this.store.details.country;
-      let stateAbr = this.store.details.state;
-      let state = this.stateNames.filter(stateName => {
-        return stateName.value.toLowerCase() === stateAbr.toLowerCase();
-      });
+    // if (this.store.details) {
+    //   this.form[1].state = this.store.details.state;
+    //   this.form[1].country = this.store.details.country;
+    //   let stateAbr = this.store.details.state;
+    //   let state = this.stateNames.filter(stateName => {
+    //     return stateName.value.toLowerCase() === stateAbr.toLowerCase();
+    //   });
 
-      this.form[1].state = state[0] ? state[0].value : null;
-    }
+    //   this.form[1].state = state[0] ? state[0].value : null;
+    // }
   },
   methods: {
     ...mapActions(["init", "setToken"]),
