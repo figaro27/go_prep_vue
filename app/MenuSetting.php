f214cbf23de2663dfca7e10c0cd5318ff8b6d6e6
figaro27 <sweetfigaro27@gmail.com>
2021-01-20 16:30:39 -0500

    no message

--- app/MenuSetting.php
+++ app/MenuSetting.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuSetting extends Model
 {
+    protected $casts = [
+        'redirectToCheckout' => 'boolean'
+    ];
+
     public function store()
     {
         return $this->belongsTo('App\Store');
