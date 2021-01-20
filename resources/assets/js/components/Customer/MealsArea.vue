0dc571dd7bedf3a9e9436a4757645a09f98d13e8
figaro27 <sweetfigaro27@gmail.com>
2021-01-20 17:22:53 -0500

    Fixing menu style on mobile

--- resources/assets/js/components/Customer/MealsArea.vue
+++ resources/assets/js/components/Customer/MealsArea.vue
@@ -184,7 +184,6 @@
       ref="packageComponentModal"
       :packageTitle="packageTitle"
     ></meal-package-components-modal>
-
     <div
       v-for="(group, catIndex) in meals"
       :key="'category_' + group.category + '_' + catIndex"
@@ -199,7 +198,7 @@
           (isVisible, entry) => $parent.onCategoryVisible(isVisible, group)
         "
       >
-        <div v-if="storeSettings.menuStyle === 'image'">
+        <div v-if="storeSettings.menuStyle === 'image' && !smallScreen">
           <h2 class="text-center mb-4 dbl-underline" v-if="hasItems(group)">
             {{ group.category }}
           </h2>
@@ -552,7 +551,7 @@
           </div>
         </div>
 
-        <div v-if="storeSettings.menuStyle === 'text'">
+        <div v-else>
           <h2 class="text-center mb-3 dbl-underline">
             {{ group.category }}
           </h2>
