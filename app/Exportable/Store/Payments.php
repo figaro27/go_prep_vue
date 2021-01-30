3d48cec051b5d09ebdddb1722eed32761569deb3
figaro27 <sweetfigaro27@gmail.com>
2021-01-30 15:47:28 -0500

    no message

--- app/Exportable/Store/Payments.php
+++ app/Exportable/Store/Payments.php
@@ -82,7 +82,9 @@ class Payments
         $payments = $orders
             ->map(function ($order) use ($columns) {
                 $columns['paid_at'] = !$order->isMultipleDelivery
-                    ? $order->paid_at
+                    ? Carbon::parse($order->paid_at)
+                        ->toDateTimeString()
+                        ->format('D, m/d/Y')
                     : 'Multiple';
                 $columns['delivery_date'] = !$order->isMultipleDelivery
                     ? $order->delivery_date->format('D, m/d/Y')
