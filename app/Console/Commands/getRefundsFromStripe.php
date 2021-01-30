debd4025a303518435add5bb206e6bb0894a38f7
figaro27 <sweetfigaro27@gmail.com>
2021-01-30 12:37:29 -0500

    no message

--- app/Console/Commands/getRefundsFromStripe.php
+++ app/Console/Commands/getRefundsFromStripe.php
@@ -49,41 +49,65 @@ class getRefundsFromStripe extends Command
             try {
                 $acct = $store->settings->stripe_account;
                 \Stripe\Stripe::setApiKey($acct['access_token']);
-                $refunds = \Stripe\Refund::all([]);
-                foreach ($refunds as $refund) {
-                    try {
-                        $individualRefund = \Stripe\Refund::retrieve(
-                            $refund['id']
-                        );
-                        $created = Carbon::createFromTimestamp(
-                            $individualRefund['created']
-                        );
 
-                        $orderTransaction = OrderTransaction::where(
-                            'stripe_id',
-                            $refund['charge']
-                        )->first();
-                        if ($orderTransaction) {
-                            $order_number = Order::where(
-                                'id',
-                                $orderTransaction->order_id
-                            )
-                                ->pluck('order_number')
-                                ->first();
-                            $newRefund = new Refund();
-                            $newRefund->created_at = $created;
-                            $newRefund->store_id = $store['id'];
-                            $newRefund->stripe_id = $refund['id'];
-                            $newRefund->charge_id = $refund['charge'];
-                            $newRefund->user_id = $orderTransaction->user_id;
-                            $newRefund->order_id = $orderTransaction->order_id;
-                            $newRefund->order_number = $order_number;
-                            $newRefund->card_id = $orderTransaction->card_id;
-                            $newRefund->amount = $refund['amount'] / 100;
-                            $newRefund->save();
+                // Looping every 3 months since launch since Stripe limits you to getting only 100 at a time.
+
+                $timestamp = Carbon::parse('2019-01-01')->timestamp;
+                $stop = false;
+
+                while (!$stop) {
+                    $refunds = \Stripe\Refund::all([
+                        'limit' => 100,
+                        'created' => [
+                            'lte' => $timestamp
+                        ]
+                    ]);
+                    foreach ($refunds as $refund) {
+                        try {
+                            $individualRefund = \Stripe\Refund::retrieve(
+                                $refund['id']
+                            );
+                            $created = Carbon::createFromTimestamp(
+                                $individualRefund['created']
+                            );
+
+                            $orderTransaction = OrderTransaction::where(
+                                'stripe_id',
+                                $refund['charge']
+                            )->first();
+                            if ($orderTransaction) {
+                                $order_number = Order::where(
+                                    'id',
+                                    $orderTransaction->order_id
+                                )
+                                    ->pluck('order_number')
+                                    ->first();
+                                $newRefund = new Refund();
+                                $newRefund->created_at = $created;
+                                $newRefund->store_id = $store['id'];
+                                $newRefund->stripe_id = $refund['id'];
+                                $newRefund->charge_id = $refund['charge'];
+                                $newRefund->user_id =
+                                    $orderTransaction->user_id;
+                                $newRefund->order_id =
+                                    $orderTransaction->order_id;
+                                $newRefund->order_number = $order_number;
+                                $newRefund->card_id =
+                                    $orderTransaction->card_id;
+                                $newRefund->amount = $refund['amount'] / 100;
+                                $newRefund->save();
+                            }
+                        } catch (\Exception $e) {
                         }
-                    } catch (\Exception $e) {
                     }
+
+                    $timestamp = Carbon::createFromTimestamp($timestamp);
+
+                    if (!$timestamp->isPast()) {
+                        $stop = true;
+                    }
+                    $timestamp->addDays(90);
+                    $timestamp = Carbon::parse($timestamp)->timestamp;
                 }
             } catch (\Exception $e) {
             }
