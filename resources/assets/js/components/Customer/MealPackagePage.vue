afc43fd31a6f9e6e757cf5f1ad509b34342b10a7
figaro27 <sweetfigaro27@gmail.com>
2021-02-06 15:50:03 -0500

    no message

--- resources/assets/js/components/Customer/MealPackagePage.vue
+++ resources/assets/js/components/Customer/MealPackagePage.vue
@@ -1037,14 +1037,14 @@ export default {
 
       let sortedDays = [];
 
-      if (
-        this.store.id !== 251 &&
-        this.store.delivery_day_zip_codes.length === 0
-      ) {
-        sortedDays = _.uniqBy(storeDeliveryDays, "day_friendly");
-      } else {
-        sortedDays = storeDeliveryDays;
-      }
+      // if (
+      //   this.store.id !== 251 &&
+      //   this.store.delivery_day_zip_codes.length === 0
+      // ) {
+      //   sortedDays = _.uniqBy(storeDeliveryDays, "day_friendly");
+      // } else {
+      //   sortedDays = storeDeliveryDays;
+      // }
 
       // If the store only serves certain zip codes on certain delivery days
       if (this.store.delivery_day_zip_codes.length > 0) {
