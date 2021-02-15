9baa1de3991be5659197e70a6927413565b39429
figaro27 <sweetfigaro27@gmail.com>
2021-02-15 14:06:01 -0500

    no message

--- resources/views/email/store/new-subscription.blade.php
+++ resources/views/email/store/new-subscription.blade.php
@@ -139,20 +139,6 @@ $currency = $subscription->store->settings->currency
                       <tr>
                         <td align="right" style="font-family: 'Open Sans', Arial, sans-serif; font-size:13px; color:#7f8c8d; line-height:26px;"> {{ $subscription->transfer_type }} Date - {{ $subscription->next_delivery_date->format($subscription->store->settings->date_format) }}</td>
                       </tr>
-                      @if ($subscription->pickup_location_id !== null)
-                      <tr>
-                        <td height="50" style="font-family: 'Open Sans', Arial, sans-serif; font-size:14px; color:#7f8c8d;"> <b>Pickup Location:</b>
-                          {{ $subscription->pickup_location->name }}, 
-                          {{ $subscription->pickup_location->address }},
-                          {{ $subscription->pickup_location->city }},
-                          {{ $subscription->pickup_location->state }},
-                          {{ $subscription->pickup_location->zip }}<br><br>
-                          @if ($subscription->pickup_location->instructions)
-                          <b>Instructions:</b> {{ $subscription->pickup_location->instructions }}
-                          @endif
-                          </td>
-                      </tr>
-                      @endif
                       @endif
                       <!-- end address -->
                       <tr>
