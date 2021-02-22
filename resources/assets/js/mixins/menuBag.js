45eded18a326e1c72bd247fd0a8f9f4dcfcb26dc
figaro27 <sweetfigaro27@gmail.com>
2021-02-22 14:56:39 -0500

    no message

--- resources/assets/js/mixins/menuBag.js
+++ resources/assets/js/mixins/menuBag.js
@@ -70,13 +70,13 @@ export default {
       // If delivery_days table has the same day of the week for both pickup & delivery, only show the day once
       let baseDeliveryDays = this.store.delivery_days;
 
-      if (this.context === "store") {
-        baseDeliveryDays.forEach(day => {
-          let m = moment(day.day_friendly);
-          let newDate = moment(m).subtract(1, "week");
-          day.day_friendly = newDate;
-        });
-      }
+      // if (this.context === "store") {
+      //   baseDeliveryDays.forEach(day => {
+      //     let m = moment(day.day_friendly);
+      //     let newDate = moment(m).subtract(1, "week");
+      //     day.day_friendly = newDate;
+      //   });
+      // }
 
       let deliveryWeeks = this.store.settings.deliveryWeeks;
       if (this.context === "store" && deliveryWeeks < 8) {
@@ -163,7 +163,6 @@ export default {
 
       // Removing past dates
       sortedDays = sortedDays.filter(day => {
-        console.log(moment().startOf("day"));
         return !moment(day.day_friendly).isBefore(moment().startOf("day"));
       });
 
