fc77a9e80ca8b092b2ef5a17cce50fcef315e30c
figaro27 <sweetfigaro27@gmail.com>
2021-02-26 14:01:39 -0500

    no message

--- app/Console/Commands/misc.php
+++ app/Console/Commands/misc.php
@@ -10,6 +10,7 @@ use App\UserDetail;
 use Illuminate\Support\Carbon;
 use App\PurchasedGiftCard;
 use App\StoreSetting;
+use App\MealMealTag;
 
 class misc extends Command
 {
@@ -44,13 +45,75 @@ class misc extends Command
      */
     public function handle()
     {
-        $storeSettings = StoreSetting::all();
-        foreach ($storeSettings as $storeSetting) {
-            $notifications = $storeSetting->notifications;
-            $notifications['delivery_today'] = true;
-            $notifications['subscription_renewing'] = true;
-            $storeSetting->notifications = $notifications;
-            $storeSetting->update();
+        $mealMealTags = MealMealTag::findMany([
+            346,
+            367,
+            447,
+            446,
+            476,
+            714,
+            713,
+            828,
+            836,
+            846,
+            936,
+            954,
+            124,
+            988,
+            1161,
+            1207,
+            1425,
+            1429,
+            1980,
+            1996,
+            2003,
+            1982,
+            2426,
+            2490,
+            1984,
+            3042,
+            3060,
+            2720,
+            2864,
+            2829,
+            2704,
+            2877,
+            2667,
+            2895,
+            2977,
+            2544,
+            3421,
+            3499,
+            2547,
+            3650,
+            3101,
+            2762,
+            4209,
+            4284,
+            4285,
+            4286,
+            4287,
+            4448,
+            4474,
+            4475,
+            4496,
+            4495,
+            5781,
+            6454,
+            6520,
+            7124,
+            7734,
+            10685,
+            11007,
+            10995,
+            9505
+        ]);
+
+        foreach ($mealMealTags as $mealMealTag) {
+            try {
+                $mealMealTag->delete();
+            } catch (\Exception $e) {
+            }
         }
     }
 }
---
+++ app/MealMealTag.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MealMealTag extends Model
+{
+    protected $table = 'meal_meal_tag';
+}
