9634ab1c48090148dec013e47588db6ddf79ec00
figaro27 <sweetfigaro27@gmail.com>
2021-03-25 16:06:11 -0400

    no message

--- resources/assets/js/components/Customer/MealPackagePage.vue
+++ resources/assets/js/components/Customer/MealPackagePage.vue
@@ -1840,13 +1840,12 @@ export default {
           }
           this.$set(this.choices[component.id], option.id, truncated);
         } else if (remaining == 0) {
-          alert(component.id);
           // Next Part
           let elem = $(
             '.categoryNavItem[target="categorySection_' + component.id + '"]'
           ).next();
+          alert(JSON.stringify(elem));
           if (elem) {
-            alert("b");
             elem.click();
           }
         }
