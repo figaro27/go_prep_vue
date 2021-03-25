dfb8545708183e03443861b8538239766925d403
figaro27 <sweetfigaro27@gmail.com>
2021-03-25 13:57:16 -0400

    no message

--- app/Http/Controllers/Store/PaymentController.php
+++ app/Http/Controllers/Store/PaymentController.php
@@ -11,44 +11,6 @@ class PaymentController extends StoreController
     public function getPayments(Request $request)
     {
         $filterPayments = new FilterPayments();
-
-        return $this->formatResponse($filterPayments->getPayments($request));
-    }
-
-    protected function formatResponse($orders)
-    {
-        return $orders->map(function ($order) {
-            return $order->only([
-                'id',
-                'paid_at',
-                'delivery_date',
-                'totalPayments',
-                'order_number',
-                'customer_name',
-                'totalPayments',
-                'subtotal',
-                'couponReduction',
-                'couponCode',
-                'mealPlan',
-                'salesTax',
-                'processingFee',
-                'deliveryFee',
-                'purchasedGift',
-                'gratuity',
-                'coolerDeposit',
-                'referralReduction',
-                'promotionReduction',
-                'pointsReduction',
-                'chargedAmount',
-                'preTransaction',
-                'transactionFee',
-                'amount',
-                'refundedAmount',
-                'balance',
-                'preTransactionFeeAmount',
-                'voided',
-                'mealPlanDiscount'
-            ]);
-        });
+        return $filterPayments->getPayments($request);
     }
 }
