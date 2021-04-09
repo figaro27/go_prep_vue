6c5762ce0a30843252ddc54e4ed9b8a78cde3ec3
figaro27 <sweetfigaro27@gmail.com>
2021-04-09 15:49:49 -0400

    Restricting customDeliveryDay stores to only pick their own possible delivery days in order to prevent subscription cutoff / renewal issues

--- resources/assets/js/components/Customer/CheckoutArea.vue
+++ resources/assets/js/components/Customer/CheckoutArea.vue
@@ -2582,6 +2582,10 @@ use next_delivery_dates
       return options;
     },
     deliveryDateOptionsStoreView() {
+      if (this.storeModules.customDeliveryDays) {
+        return this.deliveryDateOptions;
+      }
+
       let options = [];
       let today = new Date();
 
