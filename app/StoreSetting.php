b3aa10b2fa41ee11ab933a68cad694cdc4895f6e
figaro27 <sweetfigaro27@gmail.com>
2021-05-04 12:27:00 -0400

    no message

--- app/StoreSetting.php
+++ app/StoreSetting.php
@@ -370,10 +370,7 @@ class StoreSetting extends Model
     {
         // Hard coding for Beyond Vegan. Will modulize in future if requested. Ignores cutoff on same day before 3 PM PST.
         $factorCutoff = true;
-        if (
-            ($this->store_id === 118 || $this->store_id === 3) &&
-            (int) date('H') < 22
-        ) {
+        if ($this->store_id === 118 && (int) date('H') < 22) {
             $factorCutoff = false;
         }
 
